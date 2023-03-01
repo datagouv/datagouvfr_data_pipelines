09e9ed4e969b63a2684bf3c9a7d3789d43d6a4f2
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2023-03-01 14:39:00 +0100

    [data_processing] [insee] [geocodage] add properties SSHOperator to prevent timeout exception (due to airflow 2.5 version)

--- data_processing/insee/sirene/DAG-sirene-geocodage-etalab.py
+++ data_processing/insee/sirene/DAG-sirene-geocodage-etalab.py
@@ -18,72 +18,100 @@ with DAG(
             "cd /srv/sirene/addok-docker/ && docker-compose -f docker-compose-ban-poi.yml up "
             "--scale addok-ban=6 --scale addok-redis-ban=6 -d"
         ),
-        dag=dag
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
     )
 
     wait_addok_to_be_ready = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="wait_addok_to_be_ready",
         command="sleep 120",
-        dag=dag
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
     )
 
     download_last_sirene_batch = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="download_last_sirene_batch",
         command="/srv/sirene/geocodage-sirene/1_download_last_sirene_batch.sh ",
-        dag=dag
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
     )
 
     split_departments_files = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="split_departments_files",
         command="/srv/sirene/geocodage-sirene/2_split_departments_files.sh ",
-        dag=dag
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
     )
 
     geocoding = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="geocoding",
         command="/srv/sirene/geocodage-sirene/3_geocoding_by_increasing_size.sh ",
-        dag=dag
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
     )
 
     split_by_locality = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="split_by_locality",
         command="/srv/sirene/geocodage-sirene/4a_split_by_locality.sh ",
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     get_geocode_stats = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="get_geocode_stats",
         command="/srv/sirene/geocodage-sirene/4b_geocode_stats.sh ",
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     shutdown_addok = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="shutdown_addok",
         command="cd /srv/sirene/addok-docker/ && docker-compose down --remove-orphans ",
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     national_files_agregation = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="national_files_agregation",
         command="/srv/sirene/geocodage-sirene/5_national_files_agregation.sh ",
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     prepare_to_rsync = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="prepare_to_rsync",
         command="/srv/sirene/geocodage-sirene/6_prepare_to_rsync.sh ",
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     rsync_to_files_data_gouv = SSHOperator(
         ssh_conn_id="SSH_DATAENG_ETALAB_STUDIO",
         task_id="rsync_to_files_data_gouv",
         command="/srv/sirene/geocodage-sirene/7_rsync_to_files_data_gouv.sh ",
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     mkdir_last_and_symbolic_links = SSHOperator(
         ssh_conn_id="SSH_FILES_DATA_GOUV_FR",
@@ -94,7 +122,10 @@ with DAG(
             r"mkdir /srv/nfs/files.data.gouv.fr/geo-sirene/last/{} \; && find * -type f -exec "
             r"ln -s `pwd`/{} /srv/nfs/files.data.gouv.fr/geo-sirene/last/{} \;"
         ),
-        dag=dag)
+        dag=dag,
+        conn_timeout=(3600 * 8),
+        cmd_timeout=(3600 * 8),
+    )
 
     wait_addok_to_be_ready.set_upstream(start_addok)
     download_last_sirene_batch.set_upstream(wait_addok_to_be_ready)
