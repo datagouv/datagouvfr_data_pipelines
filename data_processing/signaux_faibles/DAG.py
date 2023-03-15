3488dab5475676ee72d3de0abd63971694a84c1e
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2023-03-15 20:55:20 +0100

    feat: add signaux faibles dag

---
+++ data_processing/signaux_faibles/DAG.py
@@ -0,0 +1,52 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from airflow.operators.bash import BashOperator
+
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from datagouvfr_data_pipelines.config import (
+    AIRFLOW_DAG_TMP,
+)
+from datagouvfr_data_pipelines.data_processing.signaux_faibles.task_functions import (
+    process_signaux_faibles,
+    send_file_to_minio,
+    compare_files_minio,
+    send_notification,
+)
+
+TMP_FOLDER = f"{AIRFLOW_DAG_TMP}signaux_faibles_ratio_financiers/"
+
+with DAG(
+    dag_id="data_processing_signaux_faibles_ratio_financiers",
+    schedule_interval="0 6 * * MON",
+    start_date=days_ago(8),
+    dagrun_timeout=timedelta(minutes=15),
+    tags=["signaux faibles", "bilans", "entreprises"],
+    params={},
+    catchup=False,
+) as dag:
+    clean_previous_outputs = BashOperator(
+        task_id="clean_previous_outputs",
+        bash_command=f"rm -rf {TMP_FOLDER} && mkdir -p {TMP_FOLDER}",
+    )
+
+    process_signaux_faibles = PythonOperator(
+        task_id="process_signaux_faibles", python_callable=process_signaux_faibles
+    )
+
+    send_file_to_minio = PythonOperator(
+        task_id="send_file_to_minio", python_callable=send_file_to_minio
+    )
+
+    compare_files_minio = ShortCircuitOperator(
+        task_id="compare_files_minio", python_callable=compare_files_minio
+    )
+
+    send_notification = PythonOperator(
+        task_id="send_notification", python_callable=send_notification
+    )
+
+    process_signaux_faibles.set_upstream(clean_previous_outputs)
+    send_file_to_minio.set_upstream(process_signaux_faibles)
+    compare_files_minio.set_upstream(send_file_to_minio)
+    send_notification.set_upstream(compare_files_minio)
---
+++ data_processing/signaux_faibles/task_functions.py
@@ -0,0 +1,99 @@
+import pandas as pd
+
+from datagouvfr_data_pipelines.config import (
+    AIRFLOW_DAG_TMP,
+    MINIO_URL,
+    MINIO_BUCKET_DATA_PIPELINE_OPEN,
+    SECRET_MINIO_DATA_PIPELINE_USER,
+    SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+)
+from datagouvfr_data_pipelines.utils.datagouv import get_resource
+from datagouvfr_data_pipelines.utils.minio import send_files, compare_files
+from datagouvfr_data_pipelines.utils.mattermost import send_message
+
+
+def process_signaux_faibles(ti):
+    get_resource(
+        resource_id="9d213815-1649-4527-9eb4-427146ef2e5b",
+        file_to_store={
+            "dest_path": f"{AIRFLOW_DAG_TMP}signaux_faibles_ratio_financiers/",
+            "dest_name": "bilans_entreprises.csv",
+        },
+    )
+    print("download done!")
+    df_bilans = pd.read_csv(
+        f"{AIRFLOW_DAG_TMP}signaux_faibles_ratio_financiers/bilans_entreprises.csv",
+        dtype=str,
+        sep=";",
+    )
+    df_bilans["chiffre_d_affaires"] = df_bilans["chiffre_d_affaires"].astype(float)
+    df_bilans["resultat_net"] = df_bilans["resultat_net"].astype(float)
+    df_bilans = df_bilans.sort_values(by=["date_cloture_exercice"])
+    df_bilans = df_bilans.drop_duplicates(subset=["siren"], keep="last")
+    df_bilans = df_bilans[["siren", "chiffre_d_affaires", "resultat_net"]]
+    df_bilans.to_csv("synthese_bilans.csv")
+
+    ti.xcom_push(key="nb_siren", value=str(df_bilans["siren"].shape[0]))
+
+
+def send_file_to_minio():
+    send_files(
+        MINIO_URL=MINIO_URL,
+        MINIO_BUCKET=MINIO_BUCKET_DATA_PIPELINE_OPEN,
+        MINIO_USER=SECRET_MINIO_DATA_PIPELINE_USER,
+        MINIO_PASSWORD=SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+        list_files=[
+            {
+                "source_path": f"{AIRFLOW_DAG_TMP}signaux_faibles_ratio_financiers/",
+                "source_name": "synthese_bilans.csv",
+                "dest_path": "signaux_faibles/new/",
+                "dest_name": "synthese_bilans.csv",
+            },
+        ],
+    )
+
+
+def compare_files_minio():
+    is_same = compare_files(
+        MINIO_URL=MINIO_URL,
+        MINIO_BUCKET=MINIO_BUCKET_DATA_PIPELINE_OPEN,
+        MINIO_USER=SECRET_MINIO_DATA_PIPELINE_USER,
+        MINIO_PASSWORD=SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+        file_path_1="signaux_faibles/new/",
+        file_name_2="synthese_bilans.csv",
+        file_path_2="signaux_faibles/latest/",
+        file_name_1="synthese_bilans.csv",
+    )
+    if is_same:
+        return False
+
+    if is_same is None:
+        print("First time in this Minio env. Creating")
+
+    send_files(
+        MINIO_URL=MINIO_URL,
+        MINIO_BUCKET=MINIO_BUCKET_DATA_PIPELINE_OPEN,
+        MINIO_USER=SECRET_MINIO_DATA_PIPELINE_USER,
+        MINIO_PASSWORD=SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+        list_files=[
+            {
+                "source_path": f"{AIRFLOW_DAG_TMP}signaux_faibles_ratio_financiers/",
+                "source_name": "synthese_bilans.csv",
+                "dest_path": "signaux_faibles/latest/",
+                "dest_name": "synthese_bilans.csv",
+            },
+        ],
+    )
+
+    return True
+
+
+def send_notification(ti):
+    nb_siren = ti.xcom_pull(key="nb_siren", task_ids="process_agence_bio")
+    send_message(
+        text=(
+            ":mega: Données Signaux faibles mises à jour.\n"
+            f"- {nb_siren} unités légales référencés\n"
+            f"- Données stockées sur Minio - Bucket {MINIO_BUCKET_DATA_PIPELINE_OPEN}"
+        )
+    )
