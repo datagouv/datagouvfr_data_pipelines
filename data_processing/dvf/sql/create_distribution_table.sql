40e2989915f1bc575264dca693eae4da5ac805da
Pierlou <pierlou.ramade@data.gouv.fr>
2023-04-14 19:13:53 +0200

    feat: segment distribution according to types

--- data_processing/dvf/sql/create_distribution_table.sql
+++ data_processing/dvf/sql/create_distribution_table.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS distribution_prix CASCADE;
 CREATE UNLOGGED TABLE distribution_prix (
 code_geo VARCHAR(20),
+type_local VARCHAR(11),
 xaxis TEXT,
 yaxis TEXT,
-PRIMARY KEY (code_geo));
+PRIMARY KEY (code_geo, type_local));
