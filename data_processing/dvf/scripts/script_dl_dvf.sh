9710bdb1bb9702f500c38f8783806c1235e67914
Pierlou <pierlou.ramade@data.gouv.fr>
2023-05-11 15:39:22 +0200

    feat: add copro file

--- data_processing/dvf/scripts/script_dl_dvf.sh
+++ data_processing/dvf/scripts/script_dl_dvf.sh
@@ -21,4 +21,7 @@ done
 
 find $DATADIR -name '*.gz' -exec gunzip -f '{}' \;
 
+URL_copro="https://www.data.gouv.fr/fr/datasets/r/3ea8e2c3-0038-464a-b17e-cd5c91f65ce2"
+wget $URL_copro -O $DATADIR/copro.csv
+
 cd $DATADIR && rm *.gz && ls -lh
