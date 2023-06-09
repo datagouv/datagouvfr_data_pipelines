71229573e5b49e5fd90c49f1bfd315c7594eba3e
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2023-06-09 18:42:02 +0200

    [data_processing] [dvf] remove: primary key on table stats_dvf

--- data_processing/dvf/sql/create_stats_dvf_table.sql
+++ data_processing/dvf/sql/create_stats_dvf_table.sql
@@ -1,19 +1,20 @@
 DROP TABLE IF EXISTS stats_dvf CASCADE;
 CREATE UNLOGGED TABLE stats_dvf (
-code_geo VARCHAR(20),
-nb_ventes_maison INT,
-moy_prix_m2_maison INT,
-med_prix_m2_maison INT,
-nb_ventes_appartement INT,
-moy_prix_m2_appartement INT,
-med_prix_m2_appartement INT,
-nb_ventes_local INT,
-moy_prix_m2_local INT,
-med_prix_m2_local INT,
-annee_mois VARCHAR(7),
-libelle_geo VARCHAR(100),
-code_parent VARCHAR(10),
-echelle_geo VARCHAR(15);
+    code_geo VARCHAR(20),
+    nb_ventes_maison INT,
+    moy_prix_m2_maison INT,
+    med_prix_m2_maison INT,
+    nb_ventes_appartement INT,
+    moy_prix_m2_appartement INT,
+    med_prix_m2_appartement INT,
+    nb_ventes_local INT,
+    moy_prix_m2_local INT,
+    med_prix_m2_local INT,
+    annee_mois VARCHAR(7),
+    libelle_geo VARCHAR(100),
+    code_parent VARCHAR(10),
+    echelle_geo VARCHAR(15)
+);
 /* echelle_geo VARCHAR(15),
 PRIMARY KEY (echelle_geo, code_geo, annee_mois, code_parent)); */
 CREATE INDEX echelle_geo_idx ON stats_dvf USING btree (echelle_geo);
