0d8fd20b827578af6ce9fd8343248ee250281be6
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2023-07-04 17:41:10 +0200

    right URL

--- data_processing/dvf/scripts/script_dl_dpe.sh
+++ data_processing/dvf/scripts/script_dl_dpe.sh
@@ -1,7 +1,7 @@
 DATADIR="$1"
 
 URL_dpe="https://www.data.gouv.fr/fr/datasets/r/ad4bb2f6-0f40-46d2-a636-8d2604532f74"
-curl -o $DATADIR/dpe.tar.gz $URL_copro -L
+curl -o $DATADIR/dpe.tar.gz $URL_dpe -L
 
 tar -xzvf dpe.tar.gz ./csv/batiment_groupe_dpe_representatif_logement.csv ./csv/rel_batiment_groupe_parcelle.csv
 
