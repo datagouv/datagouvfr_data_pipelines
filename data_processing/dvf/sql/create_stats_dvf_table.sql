657b861fad388a783c8fe9d88ad04c11cf7f23b4
Pierlou <pierlou.ramade@data.gouv.fr>
2023-07-04 10:45:45 +0200

    feat: 5 years stats for sections, better performances, apt_maison in 5 years

--- data_processing/dvf/sql/create_stats_dvf_table.sql
+++ data_processing/dvf/sql/create_stats_dvf_table.sql
@@ -10,13 +10,15 @@ CREATE UNLOGGED TABLE dvf.stats_dvf (
     nb_ventes_local INT,
     moy_prix_m2_local INT,
     med_prix_m2_local INT,
+    nb_ventes_apt_maison INT,
+    moy_prix_m2_apt_maison INT,
+    med_prix_m2_apt_maison INT,
     annee_mois VARCHAR(7),
     libelle_geo VARCHAR(100),
     code_parent VARCHAR(10),
     echelle_geo VARCHAR(15)
 );
-/* echelle_geo VARCHAR(15),
-PRIMARY KEY (echelle_geo, code_geo, annee_mois, code_parent)); */
+/* PRIMARY KEY (echelle_geo, code_geo, annee_mois, code_parent)); */
 CREATE INDEX echelle_geo_idx ON stats_dvf USING btree (echelle_geo);
 CREATE INDEX code_geo_idx ON stats_dvf USING btree (code_geo);
 CREATE INDEX code_parent_idx ON stats_dvf USING btree (code_parent);
