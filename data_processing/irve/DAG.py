171dcb33ce4e1ba2913a89ec5c1276365ecf2389
Pierlou <pierlou.ramade@data.gouv.fr>
2023-07-13 14:20:21 +0200

    feat: complete DAG IRVE

--- data_processing/irve/DAG.py
+++ data_processing/irve/DAG.py
@@ -5,44 +5,59 @@ from operators.mattermost import MattermostOperator
 from operators.python_minio import PythonMinioOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta, datetime
-
+from pathlib import Path
 import os
-import requests
-import pandas as pd
 
 from datagouvfr_data_pipelines.config import (
-    AIRFLOW_DAG_HOME,
     AIRFLOW_DAG_TMP,
     MINIO_URL,
     MINIO_BUCKET_DATA_PIPELINE_OPEN,
     SECRET_MINIO_DATA_PIPELINE_USER,
     SECRET_MINIO_DATA_PIPELINE_PASSWORD,
     MATTERMOST_DATAGOUV_SCHEMA_ACTIVITE,
-    DATAGOUV_SECRET_API_KEY,
-    DATAGOUV_URL
+    DATAGOUV_SECRET_API_KEY
 )
-from datagouvfr_data_pipelines.utils.minio import send_files
-from datagouvfr_data_pipelines.utils.mattermost import send_message
+from datagouvfr_data_pipelines.data_processing.irve.task_functions import (
+    get_all_irve_resources,
+    download_irve_resources,
+    consolidate_irve,
+    upload_consolidated_irve,
+    update_reference_table_irve,
+    update_resource_send_mail_producer_irve,
+    add_validata_report_irve,
+    update_consolidation_documentation_report_irve,
+    create_consolidation_reports_irve,
+    create_detailed_report_irve,
+    final_directory_clean_up_irve,
+    notification_synthese_irve
+)
+from datagouvfr_data_pipelines.data_processing.irve.geo_utils.geo import (
+    improve_geo_data_quality,
+)
+from datagouvfr_data_pipelines.utils.datagouv import (
+    DATAGOUV_URL,
+)
 
 # for local dev in order not to mess up with production
-# DATAGOUV_URL = 'https://data.gouv.fr'
-# DATAGOUV_SECRET_API_KEY = 'non'
+DATAGOUV_URL = 'https://data.gouv.fr'
+DATAGOUV_SECRET_API_KEY = 'non'
 
 DAG_NAME = "irve_consolidation"
-TMP_FOLDER = f"{AIRFLOW_DAG_TMP}{DAG_NAME}/"
+TMP_FOLDER = Path(f"{AIRFLOW_DAG_TMP}{DAG_NAME}/")
+TMP_CONFIG_FILE = TMP_FOLDER / "schema.data.gouv.fr/config_consolidation.yml"
 SCHEMA_CATALOG = "https://schema.data.gouv.fr/schemas/schemas.json"
 API_URL = f"{DATAGOUV_URL}/api/1/"
 GIT_REPO = "git@github.com:etalab/schema.data.gouv.fr.git"
-TMP_CONFIG_FILE = (
-    f"{TMP_FOLDER}schema.data.gouv.fr/config_consolidation.yml"
-)
+output_data_folder = f"{TMP_FOLDER}output/"
+date_airflow = "{{ ds }}"
 
 default_args = {
     'email': [
         'pierlou.ramade@data.gouv.fr',
         'geoffrey.aldebert@data.gouv.fr'
     ],
-    'email_on_failure': True
+    'email_on_failure': False,
+    'provide_context': True,
 }
 
 
@@ -61,30 +76,43 @@ with DAG(
 
     clone_dag_schema_repo = BashOperator(
         task_id="clone_dag_schema_repo",
-        bash_command=f"cd {TMP_FOLDER} && git clone {GIT_REPO} --depth 1 ",
+        # bash_command=f"cd {TMP_FOLDER} && git clone {GIT_REPO} --depth 1 ",
         # for local dev without SSH enabled
-        # bash_command=f"cd {TMP_FOLDER} && git clone https://github.com/etalab/schema.data.gouv.fr.git --depth 1 ",
+        bash_command=f"cd {TMP_FOLDER} && git clone https://github.com/etalab/schema.data.gouv.fr.git --depth 1 ",
     )
 
-    working_dir = f"{AIRFLOW_DAG_HOME}datagouvfr_data_pipelines/schema/scripts/"
     date_airflow = "{{ ds }}"
 
-    run_consolidation = PythonOperator(
-        task_id="run_schemas_consolidation",
-        python_callable=run_schemas_consolidation,
-        op_args=(
-            API_URL,
-            working_dir,
-            TMP_FOLDER,
-            date_airflow,
-            SCHEMA_CATALOG,
-            TMP_CONFIG_FILE,
-        ),
+    get_all_irve_resources = PythonOperator(
+        task_id="get_all_irve_resources",
+        python_callable=get_all_irve_resources,
+        op_kwargs={
+            "date_airflow": date_airflow,
+            "tmp_path": TMP_FOLDER,
+            "schemas_catalogue_url": SCHEMA_CATALOG,
+            "config_path": TMP_CONFIG_FILE
+        },
     )
 
-    schema_irve_path = os.path.join(
-        TMP_FOLDER, "consolidated_data", "etalab_schema-irve-statique"
+    download_irve_resources = PythonOperator(
+        task_id="download_irve_resources",
+        python_callable=download_irve_resources,
     )
+
+    consolidate_irve = PythonOperator(
+        task_id="consolidate_irve",
+        python_callable=consolidate_irve,
+        op_kwargs={
+            "tmp_path": TMP_FOLDER,
+            "schemas_catalogue_url": SCHEMA_CATALOG
+        },
+    )
+
+    schema_irve_path = TMP_FOLDER / "consolidated_data" / "etalab_schema-irve-statique"
+    # it will exist for sure at this point but otherwise airflow throws an error
+    if schema_irve_path.exists():
+        schema_irve_path = schema_irve_path / os.listdir(schema_irve_path)[0]
+
     schema_irve_cols = {
         "xy_coords": "coordonneesXY",
         "code_insee": "code_insee_commune",
@@ -95,43 +123,79 @@ with DAG(
 
     geodata_quality_improvement = PythonOperator(
         task_id="geodata_quality_improvement",
-        python_callable=lambda schema_path: improve_geo_data_quality(
-            {
-                os.path.join(schema_path, filename): schema_irve_cols
-                for filename in os.listdir(schema_path)
-            }
-        )
-        if schema_path is not None
-        else None,
-        op_args=[schema_irve_path],
+        python_callable=improve_geo_data_quality,
+        op_kwargs={
+            "file_cols_mapping": {schema_irve_path.as_posix(): schema_irve_cols}
+        },
     )
 
-    output_data_folder = f"{TMP_FOLDER}output/"
-    upload_consolidation = PythonMinioOperator(
-        task_id="upload_consolidated_datasets",
-        tmp_path=TMP_FOLDER,
-        minio_url=MINIO_URL,
-        minio_bucket=MINIO_BUCKET_DATA_PIPELINE_OPEN,
-        minio_user=SECRET_MINIO_DATA_PIPELINE_USER,
-        minio_password=SECRET_MINIO_DATA_PIPELINE_PASSWORD,
-        minio_output_filepath="schema/schemas_consolidation/{{ ds }}/",
-        python_callable=run_consolidation_upload,
-        op_args=(
-            API_URL,
-            DATAGOUV_SECRET_API_KEY,
-            TMP_FOLDER,
-            working_dir,
-            date_airflow,
-            SCHEMA_CATALOG,
-            output_data_folder,
-            TMP_CONFIG_FILE,
-        ),
+    upload_consolidated_irve = PythonOperator(
+        task_id="upload_consolidated_irve",
+        python_callable=upload_consolidated_irve,
+        op_kwargs={
+            "api_key": DATAGOUV_SECRET_API_KEY,
+            "config_path": TMP_CONFIG_FILE,
+            "single_schema": True
+        },
+    )
+
+    update_reference_table_irve = PythonOperator(
+        task_id="update_reference_table_irve",
+        python_callable=update_reference_table_irve,
+    )
+
+    update_resource_send_mail_producer_irve = PythonOperator(
+        task_id="update_resource_send_mail_producer_irve",
+        python_callable=update_resource_send_mail_producer_irve,
+        op_kwargs={
+            "api_key": DATAGOUV_SECRET_API_KEY
+        },
+    )
+
+    add_validata_report_irve = PythonOperator(
+        task_id="add_validata_report_irve",
+        python_callable=add_validata_report_irve,
+        op_kwargs={
+            "api_key": DATAGOUV_SECRET_API_KEY
+        },
+    )
+
+    update_consolidation_documentation_report_irve = PythonOperator(
+        task_id="update_consolidation_documentation_report_irve",
+        python_callable=update_consolidation_documentation_report_irve,
+        op_kwargs={
+            "api_key": DATAGOUV_SECRET_API_KEY,
+            "config_path": TMP_CONFIG_FILE,
+            "single_schema": True
+        },
+    )
+
+    create_consolidation_reports_irve = PythonOperator(
+        task_id="create_consolidation_reports_irve",
+        python_callable=create_consolidation_reports_irve,
+        op_kwargs={
+            "config_path": TMP_CONFIG_FILE,
+        },
+    )
+
+    create_detailed_report_irve = PythonOperator(
+        task_id="create_detailed_report_irve",
+        python_callable=create_detailed_report_irve,
+    )
+
+    final_directory_clean_up_irve = PythonOperator(
+        task_id="final_directory_clean_up_irve",
+        python_callable=final_directory_clean_up_irve,
+        op_kwargs={
+            "tmp_path": TMP_FOLDER,
+            "output_data_folder": output_data_folder,
+        },
     )
 
     commit_changes = BashOperator(
         task_id="commit_changes",
         bash_command=(
-            f"cd {TMP_FOLDER}schema.data.gouv.fr/ && git add config_consolidation.yml "
+            f"cd {TMP_FOLDER.as_posix()}/schema.data.gouv.fr/ && git add config_consolidation.yml "
             ' && git commit -m "Update config consolidation file - '
             f'{ datetime.today().strftime("%Y-%m-%d")}'
             '" || echo "No changes to commit"'
@@ -139,15 +203,32 @@ with DAG(
         )
     )
 
-    notification_synthese = PythonOperator(
-        task_id="notification_synthese",
-        python_callable=notification_synthese,
+    notification_synthese_irve = PythonOperator(
+        task_id="notification_synthese_irve",
+        python_callable=notification_synthese_irve,
+        op_kwargs={
+            "MINIO_URL": MINIO_URL,
+            "MINIO_BUCKET_DATA_PIPELINE_OPEN": MINIO_BUCKET_DATA_PIPELINE_OPEN,
+            "TMP_FOLDER": TMP_FOLDER,
+            "SECRET_MINIO_DATA_PIPELINE_USER": SECRET_MINIO_DATA_PIPELINE_USER,
+            "SECRET_MINIO_DATA_PIPELINE_PASSWORD": SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+            "MATTERMOST_DATAGOUV_SCHEMA_ACTIVITE": MATTERMOST_DATAGOUV_SCHEMA_ACTIVITE,
+        },
         templates_dict={"TODAY": "{{ ds }}"},
     )
 
     clone_dag_schema_repo.set_upstream(clean_previous_outputs)
-    run_consolidation.set_upstream(clone_dag_schema_repo)
-    geodata_quality_improvement.set_upstream(run_consolidation)
-    upload_consolidation.set_upstream(geodata_quality_improvement)
-    commit_changes.set_upstream(upload_consolidation)
-    notification_synthese.set_upstream(commit_changes)
+    get_all_irve_resources.set_upstream(clone_dag_schema_repo)
+    download_irve_resources.set_upstream(get_all_irve_resources)
+    consolidate_irve.set_upstream(download_irve_resources)
+    geodata_quality_improvement.set_upstream(consolidate_irve)
+    upload_consolidated_irve.set_upstream(geodata_quality_improvement)
+    update_reference_table_irve.set_upstream(upload_consolidated_irve)
+    update_resource_send_mail_producer_irve.set_upstream(update_reference_table_irve)
+    add_validata_report_irve.set_upstream(update_resource_send_mail_producer_irve)
+    update_consolidation_documentation_report_irve.set_upstream(add_validata_report_irve)
+    create_consolidation_reports_irve.set_upstream(update_consolidation_documentation_report_irve)
+    create_detailed_report_irve.set_upstream(create_consolidation_reports_irve)
+    final_directory_clean_up_irve.set_upstream(create_detailed_report_irve)
+    commit_changes.set_upstream(final_directory_clean_up_irve)
+    notification_synthese_irve.set_upstream(commit_changes)
--- data_processing/irve/task_functions.py
+++ data_processing/irve/task_functions.py
@@ -1,27 +1,331 @@
-from airflow.hooks.base import BaseHook
-from datagouvfr_data_pipelines.config import (
-    AIRFLOW_DAG_HOME,
-    AIRFLOW_DAG_TMP,
-    DATAGOUV_SECRET_API_KEY,
-    AIRFLOW_ENV,
-    MINIO_URL,
-    MINIO_BUCKET_DATA_PIPELINE_OPEN,
-    SECRET_MINIO_DATA_PIPELINE_USER,
-    SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+from datagouvfr_data_pipelines.utils.schema import (
+    remove_old_schemas,
+    get_schema_report,
+    build_reference_table,
+    download_schema_files,
+    consolidate_data,
+    upload_consolidated,
+    update_reference_table,
+    update_resource_send_mail_producer,
+    add_validata_report,
+    update_consolidation_documentation_report,
+    append_stats_list,
+    create_detailed_report,
+    final_directory_clean_up,
+    notification_synthese
 )
+import yaml
+from ast import literal_eval
+import pandas as pd
+import os
 
-DAG_FOLDER = "datagouvfr_data_pipelines/data_processing/"
-VALIDATA_BASE_URL = (
-    "https://validata-api.app.etalab.studio/validate?schema={schema_url}&url={rurl}"
-)
 schema_name = "etalab/schema-irve-statique"
 
 
+def get_all_irve_resources(
+    ti,
+    date_airflow,
+    tmp_path,
+    schemas_catalogue_url,
+    config_path
+):
+    consolidation_date_str = date_airflow.replace("-", "")
+    print(consolidation_date_str)
+
+    data_path = tmp_path / "data"
+    data_path.mkdir(parents=True, exist_ok=True)
+
+    consolidated_data_path = tmp_path / "consolidated_data"
+    consolidated_data_path.mkdir(parents=True, exist_ok=True)
+
+    ref_tables_path = tmp_path / "ref_tables"
+    ref_tables_path.mkdir(parents=True, exist_ok=True)
+
+    report_tables_path = tmp_path / "report_tables"
+    report_tables_path.mkdir(parents=True, exist_ok=True)
+
+    output_path = tmp_path / "output"
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    validata_reports_path = tmp_path / "validata_reports"
+    validata_reports_path.mkdir(parents=True, exist_ok=True)
+
+    schemas_report_dict, schemas_catalogue_list = get_schema_report(
+        schemas_catalogue_url=schemas_catalogue_url,
+        config_path=config_path,
+        schema_name=schema_name
+    )
+
+    with open(config_path, "r") as f:
+        config_dict = yaml.safe_load(f)
+        config_dict = remove_old_schemas(config_dict, schemas_catalogue_list, single_schema=True)
+
+    success = build_reference_table(
+        config_dict,
+        schema_name,
+        schemas_catalogue_list,
+        schemas_report_dict,
+        validata_reports_path,
+        ref_tables_path,
+        should_succeed=True
+    )
+    assert success
+
+    ti.xcom_push(key='consolidation_date_str', value=consolidation_date_str)
+    ti.xcom_push(key='data_path', value=data_path.as_posix())
+    ti.xcom_push(key='consolidated_data_path', value=consolidated_data_path.as_posix())
+    ti.xcom_push(key='ref_tables_path', value=ref_tables_path.as_posix())
+    ti.xcom_push(key='report_tables_path', value=report_tables_path.as_posix())
+    ti.xcom_push(key='validata_reports_path', value=validata_reports_path.as_posix())
+    ti.xcom_push(key='schemas_report_dict', value=str(schemas_report_dict))
+    ti.xcom_push(key='schemas_catalogue_list', value=schemas_catalogue_list)
+    return
 
 
+def download_irve_resources(ti):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    data_path = ti.xcom_pull(key='data_path', task_ids='get_all_irve_resources')
+    success = download_schema_files(
+        schema_name,
+        ref_tables_path,
+        data_path,
+        should_succeed=True
+    )
+    assert success
+    return
 
 
+def consolidate_irve(
+    ti,
+    tmp_path
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    data_path = ti.xcom_pull(key='data_path', task_ids='get_all_irve_resources')
+    consolidation_date_str = ti.xcom_pull(key='consolidation_date_str', task_ids='get_all_irve_resources')
+    consolidated_data_path = ti.xcom_pull(key='consolidated_data_path', task_ids='get_all_irve_resources')
+    schemas_report_dict = literal_eval(
+        ti.xcom_pull(key='schemas_report_dict', task_ids='get_all_irve_resources')
+    )
+    schemas_catalogue_list = ti.xcom_pull(key='schemas_catalogue_list', task_ids='get_all_irve_resources')
+    success = consolidate_data(
+        data_path,
+        schema_name,
+        consolidated_data_path,
+        ref_tables_path,
+        schemas_catalogue_list,
+        consolidation_date_str,
+        tmp_path,
+        schemas_report_dict,
+        should_succeed=True
+    )
+    assert success
+    return
 
 
+def upload_consolidated_irve(
+    ti,
+    api_key,
+    config_path,
+    single_schema
+):
+    consolidation_date_str = ti.xcom_pull(key='consolidation_date_str', task_ids='get_all_irve_resources')
+    consolidated_data_path = ti.xcom_pull(key='consolidated_data_path', task_ids='get_all_irve_resources')
+    schemas_report_dict = literal_eval(
+        ti.xcom_pull(key='schemas_report_dict', task_ids='get_all_irve_resources')
+    )
+    schemas_catalogue_list = ti.xcom_pull(key='schemas_catalogue_list', task_ids='get_all_irve_resources')
+
+    with open(config_path, "r") as f:
+        config_dict = yaml.safe_load(f)
+    config_dict = remove_old_schemas(config_dict, schemas_catalogue_list, single_schema)
+
+    success = upload_consolidated(
+        schema_name,
+        consolidated_data_path,
+        config_dict,
+        schemas_catalogue_list,
+        config_path,
+        schemas_report_dict,
+        consolidation_date_str,
+        single_schema,
+        api_key,
+        bool_upload_geojson=True,
+        should_succeed=True
+    )
+    assert success
+    return
 
 
+def update_reference_table_irve(
+    ti
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    success = update_reference_table(
+        ref_tables_path,
+        schema_name,
+        should_succeed=True
+    )
+    assert success
+    return
+
+
+def update_resource_send_mail_producer_irve(
+    ti,
+    api_key
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    success = update_resource_send_mail_producer(
+        ref_tables_path,
+        schema_name,
+        api_key,
+        should_succeed=False
+    )
+    assert success
+    return
+
+
+def add_validata_report_irve(
+    ti,
+    api_key
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    validata_reports_path = ti.xcom_pull(key='validata_reports_path', task_ids='get_all_irve_resources')
+    success = add_validata_report(
+        ref_tables_path,
+        validata_reports_path,
+        schema_name,
+        api_key,
+        should_succeed=True
+    )
+    assert success
+    return
+
+
+def update_consolidation_documentation_report_irve(
+    ti,
+    api_key,
+    config_path,
+    single_schema
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    schemas_catalogue_list = ti.xcom_pull(key='schemas_catalogue_list', task_ids='get_all_irve_resources')
+    consolidation_date_str = ti.xcom_pull(key='consolidation_date_str', task_ids='get_all_irve_resources')
+    success = update_consolidation_documentation_report(
+        schema_name,
+        ref_tables_path,
+        config_path,
+        schemas_catalogue_list,
+        consolidation_date_str,
+        single_schema,
+        api_key,
+        should_succeed=True
+    )
+    assert success
+    return
+
+
+def create_consolidation_reports_irve(
+    ti,
+    config_path,
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    schemas_catalogue_list = ti.xcom_pull(key='schemas_catalogue_list', task_ids='get_all_irve_resources')
+    report_tables_path = ti.xcom_pull(key='report_tables_path', task_ids='get_all_irve_resources')
+    consolidation_date_str = ti.xcom_pull(key='consolidation_date_str', task_ids='get_all_irve_resources')
+    schemas_report_dict = literal_eval(
+        ti.xcom_pull(key='schemas_report_dict', task_ids='get_all_irve_resources')
+    )
+
+    with open(config_path, "r") as f:
+        config_dict = yaml.safe_load(f)
+        config_dict = remove_old_schemas(config_dict, schemas_catalogue_list, single_schema=True)
+
+    reports_list = []
+
+    for schema_name in schemas_report_dict.keys():
+        schema_report_dict = schemas_report_dict[schema_name]
+        schema_report_dict["schema_name"] = schema_name
+        reports_list += [schema_report_dict]
+
+    reports_df = pd.DataFrame(reports_list)
+
+    reports_df = reports_df[
+        ["schema_name"] + [col for col in reports_df.columns if col != "schema_name"]
+    ].rename(
+        columns={"config_created": "new_config_created"}
+    )  # rename to drop at next launch
+
+    stats_df_list = []
+    append_stats_list(
+        ref_tables_path,
+        schema_name,
+        stats_df_list
+    )
+
+    stats_df = pd.concat(stats_df_list).reset_index(drop=True)
+
+    reports_df = reports_df.merge(stats_df, on="schema_name", how="left")
+
+    reports_df.head()
+
+    reports_df.to_excel(
+        os.path.join(
+            report_tables_path,
+            "report_by_schema_{}.xlsx".format(consolidation_date_str),
+        ),
+        index=False,
+    )
+    reports_df.to_csv(
+        os.path.join(
+            report_tables_path,
+            "report_by_schema_{}.csv".format(consolidation_date_str),
+        ),
+        index=False,
+    )
+
+
+def create_detailed_report_irve(
+    ti
+):
+    ref_tables_path = ti.xcom_pull(key='ref_tables_path', task_ids='get_all_irve_resources')
+    report_tables_path = ti.xcom_pull(key='report_tables_path', task_ids='get_all_irve_resources')
+    success = create_detailed_report(
+        ref_tables_path,
+        schema_name,
+        report_tables_path,
+        should_succeed=True
+    )
+    assert success
+    return
+
+
+def final_directory_clean_up_irve(
+    tmp_path,
+    output_data_folder
+):
+    tmp_folder = tmp_path.as_posix() + "/"
+    final_directory_clean_up(
+        tmp_folder,
+        output_data_folder
+    )
+
+
+def notification_synthese_irve(
+    MINIO_URL,
+    MINIO_BUCKET_DATA_PIPELINE_OPEN,
+    TMP_FOLDER,
+    SECRET_MINIO_DATA_PIPELINE_USER,
+    SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+    MATTERMOST_DATAGOUV_SCHEMA_ACTIVITE,
+    **kwargs
+):
+    notification_synthese(
+        MINIO_URL,
+        MINIO_BUCKET_DATA_PIPELINE_OPEN,
+        TMP_FOLDER,
+        SECRET_MINIO_DATA_PIPELINE_USER,
+        SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+        MATTERMOST_DATAGOUV_SCHEMA_ACTIVITE,
+        schema_name,
+        **kwargs
+    )
