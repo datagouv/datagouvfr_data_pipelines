e296cce0a6702d5a850ebecf31aebaa7392c9889
maudetes <maudet.estelle@gmail.com>
2023-08-28 17:38:26 +0200

    [curation DAG] Fallback on GET if HEAD fails poorly

--- dgv/monitoring/DAG-curation.py
+++ dgv/monitoring/DAG-curation.py
@@ -15,9 +15,13 @@ from datagouvfr_data_pipelines.utils.datagouv import get_all_from_api_query
 DAG_NAME = "dgv_curation"
 
 
-async def url_error(url, session):
+async def url_error(url, session, method="head"):
     try:
-        async with session.head(url, timeout=10) as r:
+        _method = getattr(session, method)
+        async with _method(url, timeout=15, allow_redirects=True) as r:
+            if r.status in [405, 500] and method == "head":
+                # HEAD might not be allowed or correctly implemented, trying with GET
+                return await url_error(url, session, method="get")
             r.raise_for_status()
         return False
     except (
@@ -25,23 +29,24 @@ async def url_error(url, session):
         AssertionError,
         asyncio.exceptions.TimeoutError
     ) as e:
-        return str(e)
+        return e.status if hasattr(e, "status") else str(e)
 
 
 async def crawl_reuses(reuses):
     async with aiohttp.ClientSession() as session:
         url_errors = await asyncio.gather(*[url_error(reuse["url"], session) for reuse in reuses])
 
-    unavailable_reuses = []
-    for reuse, error in zip(reuses, url_errors):
-        if error:
-            unavailable_reuses.append((reuse, error))
+    unavailable_reuses = [
+        (reuse, error) for reuse, error in zip(reuses, url_errors)
+        if error
+    ]
     return unavailable_reuses
 
 
 def get_unavailable_reuses(ti):
-    print(f"Fetching and checking reuses from {DATAGOUV_URL}/api/1/reuses/")
-    reuses = get_all_from_api_query(f"{DATAGOUV_URL}/api/1/reuses/?page_size=100")
+    print(f"Fetching reuse list from {DATAGOUV_URL}/api/1/reuses/")
+    reuses = list(get_all_from_api_query(f"{DATAGOUV_URL}/api/1/reuses/?page_size=100&sort=-created"))
+    print(f"Checking {len(reuses)} reuses")
     unavailable_reuses = asyncio.run(crawl_reuses(reuses))
     ti.xcom_push(key="reuses", value=unavailable_reuses)
     return True
