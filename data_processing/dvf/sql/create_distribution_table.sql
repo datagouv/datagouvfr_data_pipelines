20b7fd4440c67d3a186a61dd73e1e187d0a8e538
Pierlou <pierlou.ramade@data.gouv.fr>
2023-10-18 16:51:00 +0200

    refactor: truncate tables instead of drop

--- data_processing/dvf/sql/create_distribution_table.sql
+++ data_processing/dvf/sql/create_distribution_table.sql
@@ -1,7 +1,1 @@
-DROP TABLE IF EXISTS dvf.distribution_prix CASCADE;
-CREATE UNLOGGED TABLE dvf.distribution_prix (
-code_geo VARCHAR(20),
-type_local VARCHAR(11),
-xaxis TEXT,
-yaxis TEXT,
-PRIMARY KEY (code_geo, type_local));
+TRUNCATE TABLE dvf.distribution_prix;
