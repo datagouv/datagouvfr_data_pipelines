10c008a6e18472004d10f6fe3ed1ef71dacebe39
Pierlou <pierlou.ramade@data.gouv.fr>
2023-10-26 17:06:54 +0200

    feat: tasks up to sync between FTP and Minio

--- data_processing/meteo/DAG.py
+++ data_processing/meteo/DAG.py
@@ -3,18 +3,28 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
+import ftplib
 from datagouvfr_data_pipelines.config import (
     AIRFLOW_DAG_HOME,
     AIRFLOW_DAG_TMP,
+    SECRET_FTP_METEO_USER,
+    SECRET_FTP_METEO_PASSWORD,
+    SECRET_FTP_METEO_ADDRESS
 )
 from datagouvfr_data_pipelines.data_processing.meteo.task_functions import (
-    format_election_files,
+    get_current_files_on_ftp,
+    get_current_files_on_minio,
+    get_and_upload_file_diff_ftp_minio
 )
 
 TMP_FOLDER = f"{AIRFLOW_DAG_TMP}meteo/"
 DAG_FOLDER = 'datagouvfr_data_pipelines/data_processing/'
 DAG_NAME = 'data_processing_meteo'
 DATADIR = f"{AIRFLOW_DAG_TMP}meteo/data"
+minio_folder = "data/synchro_ftp/"
+
+ftp = ftplib.FTP(SECRET_FTP_METEO_ADDRESS)
+ftp.login(SECRET_FTP_METEO_USER, SECRET_FTP_METEO_PASSWORD)
 
 default_args = {
     'email': [
@@ -36,20 +46,36 @@ with DAG(
 
     clean_previous_outputs = BashOperator(
         task_id="clean_previous_outputs",
-        bash_command=f"rm -rf {TMP_FOLDER} && mkdir -p {TMP_FOLDER}",
+        bash_command=f"rm -rf {TMP_FOLDER} && mkdir -p {DATADIR}",
     )
 
-    download_elections_data = BashOperator(
-        task_id='download_elections_data',
-        bash_command=(
-            f"bash {AIRFLOW_DAG_HOME}{DAG_FOLDER}"
-            f"elections/scripts/script_dl_elections.sh {DATADIR} "
-        )
+    get_current_files_on_ftp = PythonOperator(
+        task_id='get_current_files_on_ftp',
+        python_callable=get_current_files_on_ftp,
+        op_kwargs={
+            "ftp": ftp,
+        },
     )
 
-    format_election_files = PythonOperator(
-        task_id='format_election_files',
-        python_callable=format_election_files,
+    get_current_files_on_minio = PythonOperator(
+        task_id='get_current_files_on_minio',
+        python_callable=get_current_files_on_minio,
+        op_kwargs={
+            "minio_folder": minio_folder,
+        },
     )
 
-    download_elections_data.set_upstream(clean_previous_outputs)
+    get_and_upload_file_diff_ftp_minio = PythonOperator(
+        task_id='get_and_upload_file_diff_ftp_minio',
+        python_callable=get_and_upload_file_diff_ftp_minio,
+        op_kwargs={
+            "minio_folder": minio_folder,
+            "ftp": ftp,
+        },
+    )
+
+    get_current_files_on_ftp.set_upstream(clean_previous_outputs)
+    get_current_files_on_minio.set_upstream(clean_previous_outputs)
+
+    get_and_upload_file_diff_ftp_minio.set_upstream(get_current_files_on_ftp)
+    get_and_upload_file_diff_ftp_minio.set_upstream(get_current_files_on_minio)
--- data_processing/meteo/task_functions.py
+++ data_processing/meteo/task_functions.py
@@ -4,12 +4,8 @@ from datagouvfr_data_pipelines.config import (
     DATAGOUV_SECRET_API_KEY,
     AIRFLOW_ENV,
     MINIO_URL,
-    MINIO_BUCKET_DATA_PIPELINE_OPEN,
     SECRET_MINIO_DATA_PIPELINE_USER,
     SECRET_MINIO_DATA_PIPELINE_PASSWORD,
-    SECRET_FTP_METEO_USER,
-    SECRET_FTP_METEO_PASSWORD,
-    SECRET_FTP_METEO_ADDRESS
 )
 from datagouvfr_data_pipelines.utils.datagouv import (
     post_remote_resource,
@@ -17,58 +13,105 @@ from datagouvfr_data_pipelines.utils.datagouv import (
     DATAGOUV_URL
 )
 from datagouvfr_data_pipelines.utils.mattermost import send_message
-from datagouvfr_data_pipelines.utils.minio import send_files
-import numpy as np
+from datagouvfr_data_pipelines.utils.minio import send_files, get_all_files_from_parent_folder
+import ftplib
 import os
-import pandas as pd
 import json
-from itertools import chain
 from datetime import datetime
 
 DAG_FOLDER = "datagouvfr_data_pipelines/data_processing/"
 DATADIR = f"{AIRFLOW_DAG_TMP}meteo/data"
 
 
-def send_results_to_minio():
-    send_files(
+def list_ftp_files_recursive(ftp, path='', base_path=''):
+    files = []
+    try:
+        ftp.cwd(path)
+        current_path = f"{base_path}/{path}" if base_path else path
+        ftp.retrlines('LIST', lambda x: files.append((current_path.split('//')[-1], x.split()[-1])))
+        for item in files:
+            if '.' not in item[1]:
+                files += list_ftp_files_recursive(ftp, f"{path}/{item[1]}", current_path)
+    except ftplib.error_perm:
+        pass
+    return files
+
+
+def get_current_files_on_ftp(ti, ftp):
+    ftp_files = list_ftp_files_recursive(ftp)
+    print(ftp_files)
+    ftp_files = [
+        path + '/' + file for path, file in ftp_files if '.' in file
+    ]
+    ti.xcom_push(key='ftp_files', value=ftp_files)
+
+
+def get_current_files_on_minio(ti, minio_folder):
+    minio_files = get_all_files_from_parent_folder(
         MINIO_URL=MINIO_URL,
-        MINIO_BUCKET=MINIO_BUCKET_DATA_PIPELINE_OPEN,
+        MINIO_BUCKET="meteofrance",
         MINIO_USER=SECRET_MINIO_DATA_PIPELINE_USER,
         MINIO_PASSWORD=SECRET_MINIO_DATA_PIPELINE_PASSWORD,
-        list_files=[
-            {
-                "source_path": f"{DATADIR}/",
-                "source_name": f"{t}_results.csv",
-                "dest_path": "elections/",
-                "dest_name": f"{t}_results.csv",
-            } for t in ['general', 'candidats']
-        ],
+        folder=minio_folder
     )
+    ti.xcom_push(key='minio_files', value=minio_files)
 
 
-def publish_results_elections():
-    with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}elections/config/dgv.json") as fp:
-        data = json.load(fp)
-    # post_remote_resource(
-    #     api_key=DATAGOUV_SECRET_API_KEY,
-    #     remote_url=f"https://object.files.data.gouv.fr/{MINIO_BUCKET_DATA_PIPELINE_OPEN}/{AIRFLOW_ENV}/elections/general_results.csv",
-    #     dataset_id=data["general"][AIRFLOW_ENV]["dataset_id"],
-    #     resource_id=data["general"][AIRFLOW_ENV]["resource_id"],
-    #     filesize=os.path.getsize(os.path.join(DATADIR, "general_results.csv")),
-    #     title="Résultats généraux",
-    #     format="csv",
-    #     description=f"Résultats généraux des élections agrégés au niveau des bureaux de votes, créés à partir des données du Ministère de l'Intérieur (dernière modification : {datetime.today()})",
-    # )
+def get_and_upload_file_diff_ftp_minio(ti, minio_folder, ftp):
+    minio_files = ti.xcom_pull(key='minio_files', task_ids='get_current_files_on_minio')
+    minio_files = [f.replace(minio_folder, '') for f in minio_files]
+
+    ftp_files = ti.xcom_pull(key='ftp_files', task_ids='get_current_files_on_ftp')
+    diff_files = [f for f in ftp_files if f not in minio_files]
+
+    # doing it one file at a time in order not to overload production server
+    for file_to_transfer in diff_files:
+        print(f"\nTransfering {file_to_transfer}...")
+        # we are recreating the file structure from FTP to Minio
+        path_to_file = '/'.join(file_to_transfer.split('/')[:-1])
+        file_name = file_to_transfer.split('/')[-1]
+        ftp.cwd('/' + path_to_file)
+        ftp.pwd()
+        # downaloading the file from FTP
+        with open(DATADIR + '/' + file_name, 'wb') as local_file:
+            ftp.retrbinary('RETR ' + file_name, local_file.write)
+
+        # sending file to Minio
+        try:
+            send_files(
+                MINIO_URL=MINIO_URL,
+                MINIO_BUCKET="meteofrance",
+                MINIO_USER=SECRET_MINIO_DATA_PIPELINE_USER,
+                MINIO_PASSWORD=SECRET_MINIO_DATA_PIPELINE_PASSWORD,
+                list_files=[
+                    {
+                        "source_path": f"{DATADIR}/",
+                        "source_name": file_name,
+                        "dest_path": minio_folder + path_to_file + '/',
+                        "dest_name": file_name,
+                    }
+                ],
+                ignore_airflow_env=True
+            )
+        except:
+            print("⚠️ Unable to send file")
+        os.remove(f"{DATADIR}/{file_name}")
+    ti.xcom_push(key='diff_files', value=diff_files)
 
 
-def send_notification():
-    with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}elections/config/dgv.json") as fp:
-        data = json.load(fp)
-    # send_message(
-    #     text=(
-    #         ":mega: Données élections mises à jour.\n"
-    #         f"- Données stockées sur Minio - Bucket {MINIO_BUCKET_DATA_PIPELINE_OPEN}"
-    #         f"- Données référencées [sur data.gouv.fr]({DATAGOUV_URL}/fr/datasets/"
-    #         f"{data['general'][AIRFLOW_ENV]['dataset_id']})"
-    #     )
-    # )
+# def upload_files_datagouv(ti, minio_folder):
+#     with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}dvf/config/dgv.json") as fp:
+#         config = json.load(fp)
+#     diff_files = ti.xcom_pull(key='diff_files', task_ids='get_and_upload_file_diff_ftp_minio')
+#     for file in diff_files:
+#         path_to_file = '/'.join(file.split('/')[:-1])
+
+#         post_remote_resource(
+#             api_key=DATAGOUV_SECRET_API_KEY,
+#             remote_url=f"https://object.files.data.gouv.fr/meteofrance/{minio_folder}{file}",
+#             dataset_id=config[path_to_file][AIRFLOW_ENV]["dataset_id"],
+#             # filesize=,
+#             title="Résultats généraux",
+#             format="csv.gz",
+#             description=f" (dernière modification : {datetime.today()})",
+#         )
