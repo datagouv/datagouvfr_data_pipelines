a012a33d515aee9e097764db3c661e25d36b3f79
Pierlou <pierlou.ramade@data.gouv.fr>
2023-10-30 11:57:06 +0100

    fix: truncate or recreate table according to situation

--- data_processing/dvf/sql/create_distribution_table.sql
+++ data_processing/dvf/sql/create_distribution_table.sql
@@ -1,1 +1,14 @@
-TRUNCATE TABLE dvf.distribution_prix;
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'distribution_prix' AND TABLE_SCHEMA = 'dvf') THEN
+        TRUNCATE TABLE dvf.distribution_prix;
+    ELSE
+        CREATE UNLOGGED TABLE dvf.distribution_prix (
+            code_geo VARCHAR(20),
+            type_local VARCHAR(11),
+            xaxis TEXT,
+            yaxis TEXT,
+            PRIMARY KEY (code_geo, type_local)
+        );
+    END IF;
+END $$;
--- data_processing/dvf/sql/create_stats_dvf_table.sql
+++ data_processing/dvf/sql/create_stats_dvf_table.sql
@@ -1,1 +1,30 @@
-TRUNCATE TABLE dvf.stats_dvf;
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'stats_dvf' AND TABLE_SCHEMA = 'dvf') THEN
+        TRUNCATE TABLE dvf.stats_dvf;
+    ELSE
+        CREATE UNLOGGED TABLE dvf.stats_dvf (
+            code_geo VARCHAR(20),
+            nb_ventes_maison INT,
+            moy_prix_m2_maison INT,
+            med_prix_m2_maison INT,
+            nb_ventes_appartement INT,
+            moy_prix_m2_appartement INT,
+            med_prix_m2_appartement INT,
+            nb_ventes_local INT,
+            moy_prix_m2_local INT,
+            med_prix_m2_local INT,
+            nb_ventes_apt_maison INT,
+            moy_prix_m2_apt_maison INT,
+            med_prix_m2_apt_maison INT,
+            annee_mois VARCHAR(7),
+            libelle_geo VARCHAR(100),
+            code_parent VARCHAR(10),
+            echelle_geo VARCHAR(15)
+        );
+        /* PRIMARY KEY (echelle_geo, code_geo, annee_mois, code_parent)); */
+        CREATE INDEX echelle_geo_idx ON stats_dvf USING btree (echelle_geo);
+        CREATE INDEX code_geo_idx ON stats_dvf USING btree (code_geo);
+        CREATE INDEX code_parent_idx ON stats_dvf USING btree (code_parent);
+    END IF;
+END $$;
