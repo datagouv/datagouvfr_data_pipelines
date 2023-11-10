0ed319e66a5db2e9bdd9cda81abd8d342d2b74c4
maudetes <maudet.estelle@gmail.com>
2023-11-10 11:22:48 +0100

    Target metric database and schema

--- dgv/metrics/sql/create_tables.sql
+++ dgv/metrics/sql/create_tables.sql
@@ -1,9 +1,9 @@
 -- Manually run the following:
--- CREATE SCHEMA IF NOT EXISTS airflow;
--- CREATE DATABASE airflow;
+-- CREATE SCHEMA IF NOT EXISTS metric;
+-- CREATE DATABASE metric;
 
 -- Logs visits tables
-CREATE TABLE IF NOT EXISTS airflow.visits_datasets
+CREATE TABLE IF NOT EXISTS metric.visits_datasets
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS airflow.visits_datasets
     organization_id CHARACTER VARYING,
     nb_visit INTEGER
 );
-CREATE TABLE IF NOT EXISTS airflow.visits_reuses
+CREATE TABLE IF NOT EXISTS metric.visits_reuses
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
@@ -19,14 +19,14 @@ CREATE TABLE IF NOT EXISTS airflow.visits_reuses
     organization_id CHARACTER VARYING,
     nb_visit INTEGER
 );
-CREATE TABLE IF NOT EXISTS airflow.visits_organizations
+CREATE TABLE IF NOT EXISTS metric.visits_organizations
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
     organization_id CHARACTER VARYING,
     nb_visit INTEGER
 );
-CREATE TABLE IF NOT EXISTS airflow.visits_resources
+CREATE TABLE IF NOT EXISTS metric.visits_resources
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS airflow.visits_resources
 );
 
 -- Matomo tables
-CREATE TABLE IF NOT EXISTS airflow.matomo_datasets
+CREATE TABLE IF NOT EXISTS metric.matomo_datasets
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS airflow.matomo_datasets
     organization_id CHARACTER VARYING,
     nb_outlink INTEGER
 );
-CREATE TABLE IF NOT EXISTS airflow.matomo_reuses
+CREATE TABLE IF NOT EXISTS metric.matomo_reuses
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS airflow.matomo_reuses
     organization_id CHARACTER VARYING,
     nb_outlink INTEGER
 );
-CREATE TABLE IF NOT EXISTS airflow.matomo_organizations
+CREATE TABLE IF NOT EXISTS metric.matomo_organizations
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS airflow.matomo_organizations
 
 
 -- Aggregated metrics tables
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_datasets AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_datasets AS
     SELECT visits.__id as __id,
             COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
            COALESCE(visits.dataset_id, matomo.dataset_id) as dataset_id,
@@ -71,12 +71,12 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_datasets AS
            visits.nb_visit,
            matomo.nb_outlink,
            resources.nb_visit as resource_nb_download
-    FROM airflow.visits_datasets visits
-    FULL OUTER JOIN airflow.matomo_datasets matomo
+    FROM metric.visits_datasets visits
+    FULL OUTER JOIN metric.matomo_datasets matomo
     ON visits.dataset_id = matomo.dataset_id AND
        visits.date_metric = matomo.date_metric
     LEFT OUTER JOIN (
-        SELECT dataset_id, date_metric, sum(nb_visit) as nb_visit FROM airflow.visits_resources
+        SELECT dataset_id, date_metric, sum(nb_visit) as nb_visit FROM metric.visits_resources
         GROUP BY dataset_id, date_metric
     ) resources
     ON COALESCE(visits.dataset_id, matomo.dataset_id) = resources.dataset_id AND
@@ -84,20 +84,20 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_datasets AS
 ;
 
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_reuses AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_reuses AS
     SELECT visits.__id as __id,
            COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
            COALESCE(visits.reuse_id, matomo.reuse_id) as reuse_id,
            COALESCE(visits.organization_id, matomo.organization_id) as organization_id,
            visits.nb_visit,
            matomo.nb_outlink
-    FROM airflow.visits_reuses visits
-    FULL OUTER JOIN airflow.matomo_reuses matomo
+    FROM metric.visits_reuses visits
+    FULL OUTER JOIN metric.matomo_reuses matomo
     ON visits.reuse_id = matomo.reuse_id AND
        visits.date_metric = matomo.date_metric
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_organizations AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_organizations AS
     SELECT visits.__id as __id,
            COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
            COALESCE(visits.organization_id, matomo.organization_id) as organization_id,
@@ -105,19 +105,19 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_organizations AS
            datasets.resource_nb_download as resource_nb_download,
            reuses.nb_visit as reuse_nb_visit,
            matomo.nb_outlink
-    FROM airflow.visits_organizations visits
-    FULL OUTER JOIN airflow.matomo_organizations matomo
+    FROM metric.visits_organizations visits
+    FULL OUTER JOIN metric.matomo_organizations matomo
     ON visits.organization_id = matomo.organization_id AND
        visits.date_metric = matomo.date_metric
     LEFT OUTER JOIN (
         SELECT organization_id, date_metric, sum(nb_visit) as nb_visit, sum(resource_nb_download) as resource_nb_download
-        FROM airflow.metrics_datasets
+        FROM metric.metrics_datasets
         GROUP BY organization_id, date_metric
     ) datasets
     ON COALESCE(visits.organization_id, matomo.organization_id) = datasets.organization_id AND
        COALESCE(visits.date_metric, matomo.date_metric) = datasets.date_metric
     LEFT OUTER JOIN (
-        SELECT organization_id, date_metric, sum(nb_visit) as nb_visit FROM airflow.metrics_reuses
+        SELECT organization_id, date_metric, sum(nb_visit) as nb_visit FROM metric.metrics_reuses
         GROUP BY organization_id, date_metric
     ) reuses
     ON COALESCE(visits.organization_id, matomo.organization_id) = reuses.organization_id AND
@@ -125,28 +125,28 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.metrics_organizations AS
 ;
 
 -- Monthly aggregated metrics tables
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.datasets AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.datasets AS
     SELECT
         MIN(__id) as __id,
         dataset_id,
         to_char(date_trunc('month', date_metric) , 'YYYY-mm') AS metric_month,
         sum(nb_visit) as monthly_visit,
         sum(resource_nb_download) as monthly_download_resource
-    FROM airflow.metrics_datasets
+    FROM metric.metrics_datasets
     GROUP BY metric_month, dataset_id
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.reuses AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.reuses AS
     SELECT
         MIN(__id) as __id,
         reuse_id,
         to_char(date_trunc('month', date_metric) , 'YYYY-mm') AS metric_month,
         sum(nb_visit) as monthly_visit
-    FROM airflow.metrics_reuses
+    FROM metric.metrics_reuses
     GROUP BY metric_month, reuse_id
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.organizations AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.organizations AS
     SELECT
         MIN(__id) as __id,
         organization_id,
@@ -154,23 +154,23 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.organizations AS
         sum(dataset_nb_visit) as monthly_visit_dataset,
         sum(resource_nb_download) as monthly_download_resource,
         sum(reuse_nb_visit) as monthly_visit_reuse
-    FROM airflow.metrics_organizations
+    FROM metric.metrics_organizations
     GROUP BY metric_month, organization_id
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.resources AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.resources AS
     SELECT
         MIN(__id) as __id,
         resource_id,
         dataset_id,
         to_char(date_trunc('month', date_metric) , 'YYYY-mm') AS metric_month,
         sum(nb_visit) as monthly_download_resource
-    FROM airflow.visits_resources
+    FROM metric.visits_resources
     GROUP BY metric_month, resource_id, dataset_id
 ;
 
 -- Global site table
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.site AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.site AS
     SELECT __id,
            COALESCE(datasets.metric_month, reuses.metric_month) as metric_month,
            datasets.monthly_visit as monthly_visit_dataset,
@@ -181,37 +181,37 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.site AS
                metric_month,
                sum(monthly_visit) as monthly_visit,
                sum(monthly_download_resource) as monthly_download_resource
-        FROM airflow.datasets GROUP BY metric_month ) datasets
+        FROM metric.datasets GROUP BY metric_month ) datasets
     FULL OUTER JOIN (
         SELECT metric_month,
         sum(monthly_visit) as monthly_visit
-        FROM airflow.reuses GROUP BY metric_month ) reuses
+        FROM metric.reuses GROUP BY metric_month ) reuses
     ON datasets.metric_month = reuses.metric_month
 ;
 
 -- Sum tables
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.datasets_total AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.datasets_total AS
     SELECT
         MIN(__id) as __id,
         dataset_id,
         sum(nb_visit) as visit,
         sum(nb_outlink) as outlink,
         sum(resource_nb_download) as download_resource
-    FROM airflow.metrics_datasets
+    FROM metric.metrics_datasets
     GROUP BY dataset_id
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.reuses_total AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.reuses_total AS
     SELECT
         MIN(__id) as __id,
         reuse_id,
         sum(nb_visit) as visit,
         sum(nb_outlink) as outlink
-    FROM airflow.metrics_reuses
+    FROM metric.metrics_reuses
     GROUP BY reuse_id
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.organizations_total AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.organizations_total AS
     SELECT
         MIN(__id) as __id,
         organization_id,
@@ -219,32 +219,32 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.organizations_total AS
         sum(resource_nb_download) as download_resource,
         sum(reuse_nb_visit) as visit_reuse,
         sum(nb_outlink) as outlink
-    FROM airflow.metrics_organizations
+    FROM metric.metrics_organizations
     GROUP BY organization_id
 ;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS airflow.resources_total AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.resources_total AS
     SELECT
         MIN(__id) as __id,
         resource_id,
         dataset_id,
         sum(nb_visit) as download_resource
-    FROM airflow.visits_resources
+    FROM metric.visits_resources
     GROUP BY resource_id, dataset_id
 ;
 
 
-CREATE INDEX IF NOT EXISTS visits_datasets_dataset_id ON airflow.visits_datasets USING btree (dataset_id);
-CREATE INDEX IF NOT EXISTS visits_datasets_date_metric ON airflow.visits_datasets USING btree (date_metric);
-CREATE INDEX IF NOT EXISTS visits_datasets_organization_id ON airflow.visits_datasets USING btree (organization_id);
+CREATE INDEX IF NOT EXISTS visits_datasets_dataset_id ON metric.visits_datasets USING btree (dataset_id);
+CREATE INDEX IF NOT EXISTS visits_datasets_date_metric ON metric.visits_datasets USING btree (date_metric);
+CREATE INDEX IF NOT EXISTS visits_datasets_organization_id ON metric.visits_datasets USING btree (organization_id);
 
-CREATE INDEX IF NOT EXISTS visits_organizations_organization_id ON airflow.visits_organizations USING btree (organization_id);
-CREATE INDEX IF NOT EXISTS visits_organizations_date_metric ON airflow.visits_organizations USING btree (date_metric);
+CREATE INDEX IF NOT EXISTS visits_organizations_organization_id ON metric.visits_organizations USING btree (organization_id);
+CREATE INDEX IF NOT EXISTS visits_organizations_date_metric ON metric.visits_organizations USING btree (date_metric);
 
-CREATE INDEX IF NOT EXISTS visits_reuses_reuse_id ON airflow.visits_reuses USING btree (reuse_id);
-CREATE INDEX IF NOT EXISTS visits_reuses_date_metric ON airflow.visits_reuses USING btree (date_metric);
-CREATE INDEX IF NOT EXISTS visits_reuses_organization_id ON airflow.visits_reuses USING btree (organization_id);
+CREATE INDEX IF NOT EXISTS visits_reuses_reuse_id ON metric.visits_reuses USING btree (reuse_id);
+CREATE INDEX IF NOT EXISTS visits_reuses_date_metric ON metric.visits_reuses USING btree (date_metric);
+CREATE INDEX IF NOT EXISTS visits_reuses_organization_id ON metric.visits_reuses USING btree (organization_id);
 
-CREATE INDEX IF NOT EXISTS visits_resources_resource_id ON airflow.visits_resources USING btree (resource_id);
-CREATE INDEX IF NOT EXISTS visits_resources_date_metric ON airflow.visits_resources USING btree (date_metric);
-CREATE INDEX IF NOT EXISTS visits_resources_dataset_id ON airflow.visits_resources USING btree (dataset_id);
+CREATE INDEX IF NOT EXISTS visits_resources_resource_id ON metric.visits_resources USING btree (resource_id);
+CREATE INDEX IF NOT EXISTS visits_resources_date_metric ON metric.visits_resources USING btree (date_metric);
+CREATE INDEX IF NOT EXISTS visits_resources_dataset_id ON metric.visits_resources USING btree (dataset_id);
