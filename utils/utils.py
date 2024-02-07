81e7652db234a160ee6894ef5edc2be2be1160c5
Pierlou <pierlou.ramade@data.gouv.fr>
2024-02-07 15:35:51 +0100

    feat: util to iterate on months

--- utils/utils.py
+++ utils/utils.py
@@ -16,3 +16,12 @@ def check_if_first_day_of_year():
 def get_fiscal_year(date):
     # Get the fiscal year based on the month of the date
     return date.year if date.month >= 7 else date.year - 1
+
+
+def month_year_iter(start_month, start_year, end_month, end_year):
+    # includes both start and end months
+    ym_start = 12 * start_year + start_month - 1
+    ym_end = 12 * end_year + end_month - 1
+    for ym in range(ym_start, ym_end + 1):
+        y, m = divmod(ym, 12)
+        yield y, m + 1
