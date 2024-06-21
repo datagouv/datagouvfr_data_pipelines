43cf6cb1034c5e9a4204e608b3f18c4bd1214b4b
Pierlou <pierlou.ramade@data.gouv.fr>
2024-06-21 15:47:35 +0200

    refactor

--- utils/minio.py
+++ utils/minio.py
@@ -73,11 +73,9 @@ class MinIOClient:
                     self.bucket,
                     dest_path,
                     os.path.join(file["source_path"], file["source_name"]),
-                    content_type=(
-                        file.get('content_type') or magic.from_file(
-                            os.path.join(file["source_path"], file["source_name"]),
-                            mime=True
-                        )
+                    content_type=file.get('content_type') or magic.from_file(
+                        os.path.join(file["source_path"], file["source_name"]),
+                        mime=True
                     )
                 )
             else:
