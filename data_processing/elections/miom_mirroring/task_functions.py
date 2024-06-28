fbc96c0310cb213f2b7bcb42385038f9f401da74
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2024-06-28 21:26:34 +0200

    reshape: elections folder

--- data_processing/elections/miom_mirroring/task_functions.py
+++ data_processing/elections/miom_mirroring/task_functions.py
@@ -112,7 +112,7 @@ def send_to_minio(ti):
     for cf in miom_files:
         arr.append(
             {
-                "source_path": f"{AIRFLOW_DAG_TMP}elections-mirroring/" + ID_CURRENT_ELECTION + '/' + "/".join(cf["link"].replace(URL_ELECTIONS_HTTP_SERVER, "").split("/")[:-1]) + "/",
+                "source_path": f"{AIRFLOW_DAG_TMP}elections-mirroring/" + '/' + "/".join(cf["link"].replace(URL_ELECTIONS_HTTP_SERVER, "").split("/")[:-1]) + "/",
                 "source_name": cf["name"],
                 "dest_path": "elections-mirroring/" + "/".join(cf["link"].replace(URL_ELECTIONS_HTTP_SERVER, "").split("/")[:-1]) + "/",
                 "dest_name": cf["name"],
