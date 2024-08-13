85af01e1cd3b306497025f4f22cb7cfd8e0ab608
Pierlou <pierlou.ramade@data.gouv.fr>
2024-08-13 10:48:02 +0200

    refactor: create util for months labels

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,21 @@
 import duckdb
 from datetime import date, datetime
 
+MOIS_FR = {
+    "01": "janvier",
+    "02": "février",
+    "03": "mars",
+    "04": "avril",
+    "05": "mai",
+    "06": "juin",
+    "07": "juillet",
+    "08": "août",
+    "09": "septembre",
+    "10": "octobre",
+    "11": "novembre",
+    "12": "décembre",
+}
+
 
 def check_if_monday():
     return date.today().weekday() == 0
