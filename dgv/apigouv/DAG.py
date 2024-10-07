9f734dad741c09664a41c686964e92fd86918986
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2024-10-07 11:49:10 +0200

    [DAG apigouv] fix missing import

--- dgv/apigouv/DAG.py
+++ dgv/apigouv/DAG.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
