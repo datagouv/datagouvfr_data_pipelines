8d7f949f8f5c2bdca7c81354e6c0dee9f6878d60
Pierlou <pierlou.ramade@data.gouv.fr>
2024-10-22 11:22:18 +0200

    refactor: rework of RNA consolidation

--- data_processing/rna/task_functions.py
+++ data_processing/rna/task_functions.py
@@ -1,155 +1,154 @@
-from airflow.hooks.base import BaseHook
 from datagouvfr_data_pipelines.config import (
     AIRFLOW_DAG_HOME,
     AIRFLOW_DAG_TMP,
     AIRFLOW_ENV,
     MINIO_BUCKET_DATA_PIPELINE_OPEN,
 )
-from datagouvfr_data_pipelines.utils.postgres import execute_sql_file, copy_file
 from datagouvfr_data_pipelines.utils.datagouv import (
-    post_remote_communautary_resource,
-    ORGA_REFERENCE,
+    post_remote_resource,
     DATAGOUV_URL,
 )
 from datagouvfr_data_pipelines.utils.mattermost import send_message
 from datagouvfr_data_pipelines.utils.minio import MinIOClient
+from datagouvfr_data_pipelines.utils.utils import csv_to_parquet, MOIS_FR
 import pandas as pd
 import os
-from datetime import datetime
 from unidecode import unidecode
+import requests
+import json
+from zipfile import ZipFile
+from io import BytesIO
 
 DAG_FOLDER = "datagouvfr_data_pipelines/data_processing/"
-DATADIR = f"{AIRFLOW_DAG_TMP}rna/data"
-SQLDIR = f"{AIRFLOW_DAG_TMP}rna/sql"
+DATADIR = f"{AIRFLOW_DAG_TMP}rna"
 minio_open = MinIOClient(bucket=MINIO_BUCKET_DATA_PIPELINE_OPEN)
 
 
-if AIRFLOW_ENV == "prod":
-    conn = BaseHook.get_connection("POSTGRES_DEV")
-else:
-    conn = BaseHook.get_connection("postgres_localhost")
-
-
-def process_rna():
-    # concatenate all files
-    df_rna = pd.DataFrame(None)
-    for f in sorted(os.listdir(os.path.join(DATADIR, "rna"))):
-        print(f)
-        _ = pd.read_csv(
-            os.path.join(DATADIR, "rna", f), sep=";", encoding="ISO-8859-1", dtype=str
-        )
-        df_rna = pd.concat([df_rna, _])
-    punc_to_remove = "!\"#$%&'()*+/;?@[]^_`{|}~"
-    for c in df_rna.columns:
-        df_rna[c] = df_rna[c].apply(
-            lambda s: unidecode(s)
-            .translate(str.maketrans("", "", punc_to_remove))
-            .encode("unicode-escape")
-            .decode()
-            .replace("\\", "")
-            if isinstance(s, str)
-            else s
-        )
-    # export to csv
-    df_rna.to_csv(os.path.join(DATADIR, "base_rna.csv"), index=False, encoding="utf8")
-
-
-def create_rna_table():
-    execute_sql_file(
-        conn.host,
-        conn.port,
-        conn.schema,
-        conn.login,
-        conn.password,
-        [
-            {
-                "source_path": f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}rna/sql/",
-                "source_name": "create_rna_table.sql",
-            }
-        ],
+def check_if_modif():
+    with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}rna/config/dgv.json") as fp:
+        config = json.load(fp)
+    resources = requests.get(
+        'https://www.data.gouv.fr/api/1/datasets/58e53811c751df03df38f42d/',
+        headers={"X-fields": "resources{internal{last_modified_internal}}"}
+    ).json()['resources']
+    # we consider one arbitrary resource of the target dataset
+    lastest_update = requests.get(
+        (
+            f'{DATAGOUV_URL}/api/1/datasets/{config["import"]["csv"][AIRFLOW_ENV]["dataset_id"]}/'
+            f'resources/{config["import"]["csv"][AIRFLOW_ENV]["resource_id"]}/'
+        ),
+        headers={"X-fields": "internal{last_modified_internal}"}
+    ).json()["internal"]["last_modified_internal"]
+    return any(
+        r["internal"]["last_modified_internal"] > lastest_update for r in resources
     )
 
 
-def populate_utils(files, table):
-    format_files = []
-    for file in files:
-        format_files.append(
-            {"source_path": f"{DATADIR}/", "source_name": file.split("/")[-1]}
-        )
-    copy_file(
-        PG_HOST=conn.host,
-        PG_PORT=conn.port,
-        PG_DB=conn.schema,
-        PG_TABLE=table,
-        PG_USER=conn.login,
-        PG_PASSWORD=conn.password,
-        list_files=format_files,
+def process_rna(ti, file_type):
+    assert file_type in ["import", "waldec"]
+    resources = requests.get(
+        'https://www.data.gouv.fr/api/1/datasets/58e53811c751df03df38f42d/',
+        headers={"X-fields": "resources{url}"}
+    ).json()['resources']
+    latest = sorted([r["url"] for r in resources if file_type in r["url"]])[-1]
+    r = requests.get(latest)
+    r.raise_for_status()
+    columns = None
+    with ZipFile(BytesIO(r.content)) as zip_ref:
+        for idx, file in enumerate(zip_ref.namelist()):
+            print(">", file)
+            with zip_ref.open(file) as f:
+                df = pd.read_csv(
+                    f,
+                    sep=';',
+                    dtype=str,
+                    # encoding="ISO-8859-1", # newer files are utf8-encoded
+                )
+                if columns and list(df.columns) != columns:
+                    print(columns)
+                    print(list(df.columns))
+                    raise ValueError('Columns differ between dep files')
+                columns = list(df.columns)
+                punc_to_remove = "!\"#$%&'()*+/;?@[]^_`{|}~"
+                for c in df.columns:
+                    df[c] = df[c].apply(
+                        lambda s: unidecode(s)
+                        .replace("\n", " ")
+                        .translate(str.maketrans("", "", punc_to_remove))
+                        .encode("unicode-escape")
+                        .decode()
+                        .replace("\\", "")
+                        if isinstance(s, str)
+                        else s
+                    )
+                df.to_csv(
+                    f"{DATADIR}/{file_type}.csv",
+                    index=False,
+                    encoding="utf8",
+                    mode="w" if idx == 0 else "a",
+                    header=idx == 0,
+                )
+    csv_to_parquet(
+        f"{DATADIR}/{file_type}.csv",
+        sep=',',
+        columns=columns,
     )
+    ti.xcom_push(key="latest", value=latest.split('/')[-1].split('.')[0].split('_')[2])
 
 
-def populate_rna_table():
-    if AIRFLOW_ENV == "prod":
-        populate_utils([f"{DATADIR}/base_rna.csv"], "airflow.base_rna")
-    else:
-        populate_utils([f"{DATADIR}/base_rna.csv"], "base_rna")
-
-
-def index_rna_table():
-    execute_sql_file(
-        conn.host,
-        conn.port,
-        conn.schema,
-        conn.login,
-        conn.password,
-        [
-            {
-                "source_path": f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}rna/sql/",
-                "source_name": "index_rna_table.sql",
-            }
-        ],
-    )
-
-
-def send_rna_to_minio():
+def send_rna_to_minio(file_type):
     minio_open.send_files(
         list_files=[
             {
                 "source_path": f"{DATADIR}/",
-                "source_name": "base_rna.csv",
+                "source_name": f"{file_type}.{ext}",
                 "dest_path": "rna/",
-                "dest_name": "base_rna.csv",
+                "dest_name": f"{file_type}.{ext}",
             }
+            for ext in ["csv", "parquet"]
         ],
+        ignore_airflow_env=True,
     )
 
 
-def publish_rna_communautaire():
-    file_size = os.path.getsize(os.path.join(DATADIR, "base_rna.csv"))
-    post_remote_communautary_resource(
-        dataset_id="58e53811c751df03df38f42d",
-        payload={
-            "title": "RNA agrégé",
-            "format": "csv",
-            "remote_url": (
-                f"https://object.files.data.gouv.fr/{MINIO_BUCKET_DATA_PIPELINE_OPEN}"
-                f"/{AIRFLOW_ENV}/rna/base_rna.csv"
-            ),
-            "organisation_publication_id": ORGA_REFERENCE,
-            "filesize": file_size,
-            "description": (
-                f"Répertoire National des Associations en un seul fichier,"
-                f" agrégé à partir des données brutes ({datetime.now()})"
-            ),
-        },
-        resource_id="15949cee-8e45-4614-9b82-cfea44fe2fa0",
-    )
+def publish_on_datagouv(ti, file_type):
+    latest = ti.xcom_pull(key="latest", task_ids=f"process_rna_{file_type}")
+    y, m, d = latest[:4], latest[4:6], latest[6:]
+    date = f"{d} {MOIS_FR[m]} {y}"
+    with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}rna/config/dgv.json") as fp:
+        config = json.load(fp)
+    for ext in ["csv", "parquet"]:
+        post_remote_resource(
+            dataset_id=config[file_type][ext][AIRFLOW_ENV]["dataset_id"],
+            resource_id=config[file_type][ext][AIRFLOW_ENV]["resource_id"],
+            payload={
+                "url": (
+                    f"https://object.files.data.gouv.fr/{MINIO_BUCKET_DATA_PIPELINE_OPEN}"
+                    f"/rna/{file_type}.{ext}"
+                ),
+                "filesize": os.path.getsize(DATADIR + f"/{file_type}.{ext}"),
+                "title": (
+                    f"Données {file_type.title()} au {date} (format {ext})"
+                ),
+                "format": ext,
+                "description": (
+                    f"RNA {file_type.title()} au {date} (format {ext})"
+                    " (créé à partir des [fichiers du Ministère de l'intérieur]"
+                    "(https://www.data.gouv.fr/fr/datasets/58e53811c751df03df38f42d/))"
+                ),
+            },
+        )
 
 
-def send_notification_mattermost(ti):
+def send_notification_mattermost():
+    with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}rna/config/dgv.json") as fp:
+        config = json.load(fp)
+    dataset_id = config["import"]["csv"][AIRFLOW_ENV]["dataset_id"]
+    print(non)
     send_message(
         text=(
             ":mega: Données des associations mises à jour.\n"
             f"- Données stockées sur Minio - Bucket {MINIO_BUCKET_DATA_PIPELINE_OPEN}\n"
-            f"- Données publiées [sur data.gouv.fr]({DATAGOUV_URL}/fr/datasets/58e53811c751df03df38f42d)"
+            f"- Données publiées [sur data.gouv.fr]({DATAGOUV_URL}/fr/datasets/{dataset_id})"
         )
     )
