cfa35e4cd21c53880712ffdda595627ee563031e
Pierlou <pierlou.ramade@data.gouv.fr>
2025-01-10 17:58:48 +0100

    fix: better filter for purely new resources and refactor temporary debug

--- data_processing/meteo/ftp_processing/DAG.py
+++ data_processing/meteo/ftp_processing/DAG.py
@@ -38,7 +38,8 @@ default_args = {
 
 with DAG(
     dag_id=DAG_NAME,
-    schedule_interval='30 7,10 * * *',
+    # schedule_interval='30 7,10 * * *',
+    schedule_interval='30 10 * * *',  # once a day until we spot the duplication issue
     start_date=datetime(2024, 8, 10),
     catchup=False,
     dagrun_timeout=timedelta(minutes=900),
