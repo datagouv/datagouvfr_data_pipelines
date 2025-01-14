25022d751f09e54fea2e209d789c293eed88e4c7
Pierlou <pierlou.ramade@data.gouv.fr>
2025-01-14 16:33:52 +0100

    refactor: finish refactoré

--- schema/recommendations/DAG_schemas_recommendations.py
+++ schema/recommendations/DAG_schemas_recommendations.py
@@ -3,17 +3,10 @@ from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
-from datagouvfr_data_pipelines.utils.notebook import execute_and_upload_notebook
-from datagouvfr_data_pipelines.config import (
-    AIRFLOW_DAG_HOME,
-    MINIO_URL,
-    MINIO_BUCKET_DATA_PIPELINE_OPEN,
-    SECRET_MINIO_DATA_PIPELINE_USER,
-    SECRET_MINIO_DATA_PIPELINE_PASSWORD
-)
 from datagouvfr_data_pipelines.schema.recommendations.task_functions import (
     DAG_NAME,
     TMP_FOLDER,
+    create_and_export_recommendations,
 )
 
 GIT_REPO = "git@github.com:etalab/schema.data.gouv.fr.git"
@@ -25,8 +18,9 @@ with DAG(
     dag_id=DAG_NAME,
     schedule_interval="0 4 * * *",
     start_date=datetime(2024, 8, 10),
-    dagrun_timeout=timedelta(minutes=60),
+    dagrun_timeout=timedelta(minutes=20),
     tags=["schemas", "recommendation", "schema.data.gouv.fr"],
+    catchup=False,
     default_args=default_args,
 ) as dag:
     clean_previous_outputs = BashOperator(
@@ -39,29 +33,9 @@ with DAG(
         bash_command=f"cd {TMP_FOLDER} && git clone {GIT_REPO} ",
     )
 
-    run_nb = PythonOperator(
-        task_id='run_notebook_schemas_recommendations',
-        python_callable=execute_and_upload_notebook,
-        op_kwargs={
-            "input_nb": (
-                f"{AIRFLOW_DAG_HOME}datagouvfr_data_pipelines/schema/"
-                "recommendations/schemas_recommendations.ipynb"
-            ),
-            "output_nb": "{{ ds }}.ipynb",
-            "tmp_path": f"{TMP_FOLDER}",
-            "minio_url": MINIO_URL,
-            "minio_bucket": MINIO_BUCKET_DATA_PIPELINE_OPEN,
-            "minio_user": SECRET_MINIO_DATA_PIPELINE_USER,
-            "minio_password": SECRET_MINIO_DATA_PIPELINE_PASSWORD,
-            "minio_output_filepath": "schema/schemas_recommendations/{{ ds }}/",
-            "parameters": {
-                "msgs": "Ran from Airflow {{ ds }} !",
-                "WORKING_DIR": f"{AIRFLOW_DAG_HOME}{DAG_NAME}",
-                "TMP_FOLDER": f"{TMP_FOLDER}",
-                "OUTPUT_DATA_FOLDER": f"{TMP_FOLDER}output/",
-                "DATE_AIRFLOW": "{{ ds }}",
-            },
-        },
+    create_and_export_recommendations = PythonOperator(
+        task_id='create_and_export_recommendations',
+        python_callable=create_and_export_recommendations,
     )
 
     copy_recommendations = BashOperator(
@@ -87,6 +61,6 @@ with DAG(
     )
 
     clone_schema_repo.set_upstream(clean_previous_outputs)
-    run_nb.set_upstream(clone_schema_repo)
-    copy_recommendations.set_upstream(run_nb)
+    create_and_export_recommendations.set_upstream(clone_schema_repo)
+    copy_recommendations.set_upstream(create_and_export_recommendations)
     commit_changes.set_upstream(copy_recommendations)
--- schema/recommendations/task_functions.py
+++ schema/recommendations/task_functions.py
@@ -1,44 +1,44 @@
 import json
 import requests
 import jsonschema
+import yaml
 
 from datagouvfr_data_pipelines.config import AIRFLOW_DAG_TMP
 from datagouvfr_data_pipelines.utils.datagouv import get_all_from_api_query
 
 DAG_NAME = "schema_recommendations"
 TMP_FOLDER = f"{AIRFLOW_DAG_TMP}{DAG_NAME}/"
-DATA_GOUV_API = "https://www.data.gouv.fr/api/1"
-RECOMMENDATION_SCORE = 50
-CATALOG_SCHEMAS = 'https://schema.data.gouv.fr/schemas/schemas.json'
+DATA_GOUV_API = "https://www.data.gouv.fr/api/1/"
+RECOMMENDATION_SCORE = 30
+CONFIG_CONSOLIDATION = 'https://raw.githubusercontent.com/datagouv/schema.data.gouv.fr/refs/heads/main/config_consolidation.yml'
 JSONSCHEMA_URL = "https://raw.githubusercontent.com/opendatateam/udata-recommendations/master/udata_recommendations/schema.json"
 
 
 def consolidated_schemas():
     """Find TableSchema schemas that are consolidated"""
-    r = requests.get(CATALOG_SCHEMAS)
-    schemas = r.json()['schemas']
+    config = yaml.safe_load(requests.get(CONFIG_CONSOLIDATION).content)
     return {
-        s['name']: s['consolidation_dataset_id']
-        for s in schemas
-        if s['consolidation_dataset_id'] and s['schema_type'] == 'tableschema'
+        name: params['consolidated_dataset_id']
+        for name, params in config.items()
+        if params.get('consolidated_dataset_id')
     }
 
 
 def datasets_for_schema(schema):
     """Fetch datasets on datagouv with the schema attribute set to a specific value"""
-    url = f"{DATA_GOUV_API}/datasets?schema={schema}"
+    url = f"{DATA_GOUV_API}datasets?schema={schema}"
     r = get_all_from_api_query(
         base_query=url,
-        mask='data{id}'
+        mask='data{slug}'
     )
-    return [d['id'] for d in r]
+    return [d['slug'] for d in r]
 
 
-def build_recommendation(consolidated_dataset_id, dataset_id):
+def build_recommendation(consolidated_slug, dataset_id):
     return {
         "id": dataset_id,
         "recommendations": [
-            {"id": consolidated_dataset_id, "score": RECOMMENDATION_SCORE}
+            {"id": consolidated_slug, "score": RECOMMENDATION_SCORE}
         ],
     }
 
@@ -53,19 +53,19 @@ def validate_recommendations(recommendations):
 
 def create_and_export_recommendations():
     recommendations = []
-    for schema_id, schema_details in consolidated_schemas().items():
-        consolidated_dataset_id = schema_details
+    for schema_id, consolidated_dataset_id in consolidated_schemas().items():
+        consolidated_slug = requests.get(
+            f"{DATA_GOUV_API}datasets/{consolidated_dataset_id}/"
+        ).json()["slug"]
         print(
-            f"Working on schema {schema_id}, consolidated on {consolidated_dataset_id}"
+            f"Working on schema {schema_id}, consolidated at {consolidated_dataset_id}"
         )
-
         dataset_ids = datasets_for_schema(schema_id)
         print(f"Found {len(dataset_ids)} associated with schema {schema_id}")
-
         recommendations.extend([
-            build_recommendation(consolidated_dataset_id, d) for d in dataset_ids
+            build_recommendation(consolidated_slug, d) for d in dataset_ids
         ])
-
+    # to make sure that there is not more than one recommendation per dataset (do we want this?)
     ids = []
     recommendations_clean = []
     for r in recommendations:
