4c3678f2ea8a07629644f22712dba54916f9fafe
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-02-07 15:20:57 +0100

    fix(utils.postgres): return_sql_results when nothing to fetch

--- utils/postgres.py
+++ utils/postgres.py
@@ -11,8 +11,7 @@ class File(TypedDict):
     column_order: Optional[str]
 
 
-class PostgresTool():
-
+class PostgresTool:
     def __init__(self, conn_name: str, schema: Optional[str] = None):
         airflow_conn = BaseHook.get_connection(conn_name)
         self.schema = schema or "public"
@@ -35,7 +34,7 @@ class PostgresTool():
     def execute_query(self, query: str) -> list[dict]:
         with self.conn.cursor() as cur:
             cur.execute(query)
-            data = return_sql_results(cur)
+            data = self._return_sql_results(cur)
             self.conn.commit()
         return data
 
@@ -45,7 +44,7 @@ class PostgresTool():
             cur.execute(
                 open(os.path.join(file["source_path"], file["source_name"]), "r").read()
             )
-            data = return_sql_results(cur)
+            data = self._return_sql_results(cur)
             self.conn.commit()
             return data
 
@@ -59,26 +58,26 @@ class PostgresTool():
                 ),
                 file=open(os.path.join(file["source_path"], file["source_name"]), "r"),
             )
-            data = return_sql_results(cur)
+            data = self._return_sql_results(cur)
             self.conn.commit()
         return data
 
+    @staticmethod
+    def _return_sql_results(cur) -> list[dict]:
+        """Return data from a sql query
 
-def return_sql_results(cur) -> list[dict]:
-    """Return data from a sql query
+        Args:
+            cur (Cursor): cursor from postgres connection
 
-    Args:
-        cur (Cursor): cursor from postgres connection
-
-    Returns:
-        dict, bool: result of sql query or None
-    """
-    try:
-        data = cur.fetchall()
-    except psycopg2.ProgrammingError as e:
-        print(e)
-        data = None
-    if data:
-        columns = [desc[0] for desc in cur.description]
-        return [{k: v for k, v in zip(columns, d)} for d in data]
-    return []
+        Returns:
+                  list: result of sql query, or an empty list if no result
+        """
+        try:
+            data = cur.fetchall()
+        except psycopg2.ProgrammingError as e:
+            print(e)
+            data = None
+        if data:
+            columns = [desc[0] for desc in cur.description]
+            return [{k: v for k, v in zip(columns, d)} for d in data]
+        return []
