b0370201a5c75968cef9e7a10fcd4648011754b9
Pierlou <pierlou.ramade@data.gouv.fr>
2025-02-17 12:57:16 +0100

    refactor: previsions d'ensemble have their own separate dags

--- data_processing/meteo/previsions_densemble/DAG.py
+++ data_processing/meteo/previsions_densemble/DAG.py
@@ -16,58 +16,60 @@ from datagouvfr_data_pipelines.data_processing.meteo.previsions_densemble.task_f
 TMP_FOLDER = f"{AIRFLOW_DAG_TMP}meteo_pe/"
 DAG_NAME = "data_processing_meteo_previsions_densemble"
 
-with DAG(
-    dag_id=DAG_NAME,
-    # DAG runs every 3 minutes
-    schedule_interval="*/3 * * * *",
-    start_date=datetime(2024, 6, 1),
-    catchup=False,
-    dagrun_timeout=timedelta(minutes=600),
-    tags=["data_processing", "meteo", "sftp"],
-    # runs can run in parallel, safeguards ensure they won't interfere
-    max_active_runs=4,
-) as dag:
 
-    get_files_list_on_sftp = PythonOperator(
-        task_id="get_files_list_on_sftp",
-        python_callable=get_files_list_on_sftp,
+def create_dag(pack: str, grid: str):
+    dag = DAG(
+        dag_id=DAG_NAME + f"_{pack}_{grid}",
+        # DAG runs every 3 minutes
+        schedule_interval="*/3 * * * *",
+        start_date=datetime(2024, 6, 1),
+        catchup=False,
+        dagrun_timeout=timedelta(minutes=600),
+        tags=["data_processing", "meteo", "sftp", pack],
+        # runs can run in parallel, safeguards ensure they won't interfere
+        max_active_runs=4,
     )
+    with dag:
 
-    transfers, publications, removals = [], [], []
-    for pack in CONFIG:
-        for grid in CONFIG[pack]:
-            transfers.append(
-                ShortCircuitOperator(
-                    task_id=f"transfer_{pack}_{grid}",
-                    python_callable=transfer_files_to_minio,
-                    op_kwargs={
-                        "pack": pack,
-                        "grid": grid,
-                    },
-                )
-            )
-            publications.append(
-                PythonOperator(
-                    task_id=f"publish_{pack}_{grid}",
-                    python_callable=publish_on_datagouv,
-                    op_kwargs={
-                        "pack": pack,
-                        "grid": grid,
-                    },
-                )
-            )
-            removals.append(
-                PythonOperator(
-                    task_id=f"remove_old_{pack}_{grid}",
-                    python_callable=remove_old_occurrences,
-                    op_kwargs={
-                        "pack": pack,
-                        "grid": grid,
-                    },
-                )
-            )
+        _get_files_list_on_sftp = PythonOperator(
+            task_id="get_files_list_on_sftp",
+            python_callable=get_files_list_on_sftp,
+        )
 
-    for transfer, publication, removal in zip(transfers, publications, removals):
-        transfer.set_upstream(get_files_list_on_sftp)
-        publication.set_upstream(transfer)
-        removal.set_upstream(publication)
+        _transfer_files_to_minio = ShortCircuitOperator(
+            task_id=f"transfer_{pack}_{grid}",
+            python_callable=transfer_files_to_minio,
+            op_kwargs={
+                "pack": pack,
+                "grid": grid,
+            },
+        )
+
+        _publish_on_datagouv = PythonOperator(
+            task_id=f"publish_{pack}_{grid}",
+            python_callable=publish_on_datagouv,
+            op_kwargs={
+                "pack": pack,
+                "grid": grid,
+            },
+        )
+
+        _remove_old_occurrences = PythonOperator(
+            task_id=f"remove_old_{pack}_{grid}",
+            python_callable=remove_old_occurrences,
+            op_kwargs={
+                "pack": pack,
+                "grid": grid,
+            },
+        )
+
+        _transfer_files_to_minio.set_upstream(_get_files_list_on_sftp)
+        _publish_on_datagouv.set_upstream(_transfer_files_to_minio)
+        _remove_old_occurrences.set_upstream(_publish_on_datagouv)
+    return dag
+
+
+dags = []
+for pack in CONFIG:
+    for grid in CONFIG[pack]:
+        dags.append(create_dag(pack, grid))
