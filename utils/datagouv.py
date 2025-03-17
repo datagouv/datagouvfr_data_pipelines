9f55f4f3fde2bf55b8ab53e0d155b9f5489ee8a9
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2025-03-17 11:47:53 +0100

    fix: call prod validata API

--- utils/datagouv.py
+++ utils/datagouv.py
@@ -20,7 +20,7 @@ if AIRFLOW_ENV == "dev":
 if AIRFLOW_ENV == "prod":
     DATAGOUV_URL = "https://www.data.gouv.fr"
     ORGA_REFERENCE = "646b7187b50b2a93b1ae3d45"
-VALIDATA_BASE_URL = "https://preprod-api-validata.dataeng.etalab.studio"
+VALIDATA_BASE_URL = "https://api.validata.etalab.studio/"
 DATAGOUV_MATOMO_ID = 109
 
 SPAM_WORDS = [
