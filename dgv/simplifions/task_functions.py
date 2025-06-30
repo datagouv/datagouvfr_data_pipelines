447e27ec3c61b014debe88b29d8057cf93cc05a5
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-06-30 16:38:06 +0200

    get 3 sets of subdata from grist

--- dgv/simplifions/task_functions.py
+++ dgv/simplifions/task_functions.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import requests
@@ -14,30 +15,41 @@ dgv_headers = {"X-API-KEY": DATAGOUV_SECRET_API_KEY}
 GRIST_DOC_ID = "c5pt7QVcKWWe"
 MAIN_TABLE_ID = "SIMPLIFIONS_cas_usages"
 SUBDATA_TABLE_IDS = {
-    "Solutions_publiques_recommandees": "SIMPLIFIONS_reco_solutions_cas_usages",
+    # "Solutions_publiques_recommandees": "SIMPLIFIONS_produitspublics",
+    "reco_solutions": "SIMPLIFIONS_reco_solutions_cas_usages",
     "usagers": "TYPE_usagers",
+    "A_destination_de_": "TYPE_fournisseur_service",
 }
-
 
 def get_and_format_grist_data(ti):
     rows = request_grist_table(MAIN_TABLE_ID)
     
     grist_topics = {
-        row["slug"]: {
-            key: (
-                row[key][1:]
-                # cleaning multiple-choice cells
-                if isinstance(row[key], list) and row[key][0] == "L"
-                else row[key]
-            )
-            for key in row.keys()
-        }
+        row["slug"]: get_and_format_grist_subdata(row)
         for row in rows
         if row["slug"]
     }
     ti.xcom_push(key="grist_topics", value=grist_topics)
 
-def request_grist_table(table_id: str):
+def get_and_format_grist_subdata(row):
+    formatted_row = {
+        key: handle_subdata_list(key, row[key])
+        for key in row.keys()
+    }
+    return formatted_row
+    
+def handle_subdata_list(key, value):
+    if key in SUBDATA_TABLE_IDS.keys():
+        if isinstance(value, list) and value[0] == "L":
+            filter = json.dumps({ "id": value[1:] })
+            print(filter)
+            return request_grist_table(SUBDATA_TABLE_IDS[key], filter=filter)
+        else:
+            return value
+    else:
+        return value
+
+def request_grist_table(table_id: str, filter: str = None):
     r = requests.get(
         GRIST_API_URL + f"docs/{GRIST_DOC_ID}/tables/{table_id}/records",
         headers={
@@ -45,6 +57,7 @@ def request_grist_table(table_id: str):
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
+        params={"filter": filter} if filter else None,
     )
     r.raise_for_status()
     return [row["fields"] for row in r.json()["records"]]
