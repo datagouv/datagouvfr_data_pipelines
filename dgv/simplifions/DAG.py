0078be047cb4306599c53ede1ddc14600aaecc33
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-07-30 17:02:16 +0200

    make another DAG for production

--- dgv/simplifions/DAG.py
+++ dgv/simplifions/DAG.py
@@ -2,43 +2,93 @@ from datetime import datetime, timedelta
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 
+# We need to provide the api key in the headers of our requests
+# because the client doesn't have built-in api key management
+# for the topics endpoints for now
+from datagouvfr_data_pipelines.config import (
+    DATAGOUV_SECRET_API_KEY,
+    DEMO_DATAGOUV_SECRET_API_KEY,
+)
+
+# In local, demo_client and local_client are both plugged to demo.datagouv.fr
+# So we need to fill both DATAGOUV_SECRET_API_KEY and DEMO_DATAGOUV_SECRET_API_KEY
+# with a demo api key to avoid hitting the production api.
+# In production, local_client is plugged to www.datagouv.fr
+from datagouvfr_data_pipelines.utils.datagouv import (
+  local_client,
+  demo_client,
+)
+
 from datagouvfr_data_pipelines.dgv.simplifions.task_functions import (
     get_and_format_grist_data,
     update_topics,
     update_topics_references,
 )
 
-DAG_NAME = "dgv_simplifions"
-
 default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
-with DAG(
-    dag_id=DAG_NAME,
-    schedule_interval="*/30 * * * *",
-    start_date=datetime(2024, 10, 1),
-    dagrun_timeout=timedelta(minutes=60),
-    tags=["simplifions"],
-    default_args=default_args,
-    catchup=False,
-) as dag:
+def create_simplifions_dag(dag_id=None, schedule_interval=None, client=None, api_key=None):
+    op_kwargs = {
+        "client": client,
+        "api_key": api_key,
+    }
 
-    get_and_format_grist_data = PythonOperator(
-        task_id="get_and_format_grist_data",
-        python_callable=get_and_format_grist_data,
-    )
+    with DAG(
+        dag_id=dag_id,
+        schedule_interval=schedule_interval,
+        start_date=datetime(2024, 10, 1),
+        dagrun_timeout=timedelta(minutes=60),
+        tags=["simplifions"],
+        default_args=default_args,
+        catchup=False,
+    ) as dag:
 
-    update_topics = PythonOperator(
-        task_id="update_topics",
-        python_callable=update_topics,
-    )
+        get_and_format_grist_data_task = PythonOperator(
+            task_id="get_and_format_grist_data",
+            python_callable=get_and_format_grist_data,
+            op_kwargs=op_kwargs,
+        )
 
-    update_topics_references = PythonOperator(
-        task_id="update_topics_references",
-        python_callable=update_topics_references,
-    )
+        update_topics_task = PythonOperator(
+            task_id="update_topics",
+            python_callable=update_topics,
+            op_kwargs=op_kwargs,
+        )
 
-    update_topics.set_upstream(get_and_format_grist_data)
-    update_topics_references.set_upstream(update_topics)
+        update_topics_references_task = PythonOperator(
+            task_id="update_topics_references",
+            python_callable=update_topics_references,
+            op_kwargs=op_kwargs,
+        )
+
+        update_topics_task.set_upstream(get_and_format_grist_data_task)
+        update_topics_references_task.set_upstream(update_topics_task)
+    
+    return dag
+
+dags_params = [
+    {
+        "dag_id": "dgv_simplifions_production",
+        "schedule_interval": "0 1 * * *", # every day at 1am
+        "client": local_client,
+        "api_key": DATAGOUV_SECRET_API_KEY,
+    },
+    {
+        "dag_id": "dgv_simplifions_demo",
+        "schedule_interval": "*/30 * * * *", # every 30 minutes
+        "client": demo_client,
+        "api_key": DEMO_DATAGOUV_SECRET_API_KEY,
+    },
+]
+
+dags = [
+    create_simplifions_dag(**dag_params) for dag_params in dags_params
+]
+
+
+
+
+
