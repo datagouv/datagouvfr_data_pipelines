1d99d9956469b72ba293c7785debd0109fc62e82
Pierlou <pierlou.ramade@data.gouv.fr>
2025-08-08 14:59:30 +0200

    feat: continue

--- verticales/culture/DAG.py
+++ verticales/culture/DAG.py
@@ -6,36 +6,31 @@ from airflow.operators.python import PythonOperator
 from datagouvfr_data_pipelines.config import (
     AIRFLOW_DAG_TMP,
 )
-from datagouvfr_data_pipelines.data_processing.rna.task_functions import (
+from datagouvfr_data_pipelines.vert.task_functions import (
     get_perimeter_orgas,
 )
 
-TMP_FOLDER = f"{AIRFLOW_DAG_TMP}rna/"
-DAG_FOLDER = "datagouvfr_data_pipelines/data_processing/"
+TMP_FOLDER = f"{AIRFLOW_DAG_TMP}culture/"
 DAG_NAME = "verticale_culture"
 DATADIR = f"{TMP_FOLDER}data"
 
 with DAG(
     dag_id=DAG_NAME,
-    # source files are usually uploaded in the morning of the 1st of the month
-    schedule_interval="0 12 1,2,15 * *",
-    start_date=datetime(2024, 8, 10),
+    schedule_interval=None,
+    start_date=datetime(2025, 8, 1),
     catchup=False,
     dagrun_timeout=timedelta(minutes=240),
-    tags=["data_processing", "rna", "association"],
+    tags=["verticale", "culture"],
 ) as dag:
-
-    check_if_modif = PythonOperator(
-        task_id=f"process_rna_{file_type}",
-        python_callable=process_rna,
-        op_kwargs={
-            "file_type": file_type,
-        },
-    )
 
     clean_previous_outputs = BashOperator(
         task_id="clean_previous_outputs",
         bash_command=f"rm -rf {TMP_FOLDER} && mkdir -p {TMP_FOLDER}",
     )
 
-    send_notification_mattermost.set_upstream(clean_up)
+    get_perimeter_orgas = PythonOperator(
+        task_id="get_perimeter_orgas",
+        python_callable=get_perimeter_orgas,
+    )
+
+    get_perimeter_orgas.set_upstream(clean_previous_outputs)
--- verticales/culture/task_functions.py
+++ verticales/culture/task_functions.py
@@ -3,4 +3,5 @@ from datagouvfr_data_pipelines.utils.grist import GristTable
 
 def get_perimeter_orgas(ti):
     table = GristTable("hrDZg8StuE1d", "Perimetre_culture")
+    print(table["datagouv_id"].to_list())
     ti.xcom_push(key="orgas", value=table["datagouv_id"].to_list())
