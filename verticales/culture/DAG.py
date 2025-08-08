33e5e1a180da61481885fd004c713782aca5fa86
Pierlou <pierlou.ramade@data.gouv.fr>
2025-08-08 18:08:51 +0200

    feat: build DAG until second-to-last step

--- verticales/culture/DAG.py
+++ verticales/culture/DAG.py
@@ -1,18 +1,20 @@
+from collections import defaultdict
 from datetime import datetime, timedelta
+
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-from datagouvfr_data_pipelines.config import (
-    AIRFLOW_DAG_TMP,
-)
-from datagouvfr_data_pipelines.vert.task_functions import (
+from datagouvfr_data_pipelines.verticales.culture.task_functions import (
+    DATADIR,
+    gather_stats,
+    get_and_send_perimeter_objects,
     get_perimeter_orgas,
+    get_perimeter_stats,
+    send_stats_to_minio,
 )
 
-TMP_FOLDER = f"{AIRFLOW_DAG_TMP}culture/"
 DAG_NAME = "verticale_culture"
-DATADIR = f"{TMP_FOLDER}data"
 
 with DAG(
     dag_id=DAG_NAME,
@@ -25,7 +27,7 @@ with DAG(
 
     clean_previous_outputs = BashOperator(
         task_id="clean_previous_outputs",
-        bash_command=f"rm -rf {TMP_FOLDER} && mkdir -p {TMP_FOLDER}",
+        bash_command=f"rm -rf {DATADIR} && mkdir -p {DATADIR}",
     )
 
     get_perimeter_orgas = PythonOperator(
@@ -33,4 +35,50 @@ with DAG(
         python_callable=get_perimeter_orgas,
     )
 
+    get_perimeter_stats_organizations = PythonOperator(
+        task_id="get_perimeter_stats_organizations",
+        python_callable=get_perimeter_stats,
+        op_kwargs={"object_type": "organizations"},
+    )
+
+    object_types = [
+        "datasets",
+        "dataservices",
+        "reuses",
+    ]
+    tasks = defaultdict(list)
+    for obj in object_types:
+        tasks[obj].append(
+            PythonOperator(
+                task_id=f"get_and_send_perimeter_{obj}",
+                python_callable=get_and_send_perimeter_objects,
+                op_kwargs={"object_type": obj},
+            )
+        )
+        tasks[obj].append(
+            PythonOperator(
+                task_id=f"get_perimeter_stats_{obj}",
+                python_callable=get_perimeter_stats,
+                op_kwargs={"object_type": obj},
+            )
+        )
+
+    gather_stats = PythonOperator(
+        task_id="gather_stats",
+        python_callable=gather_stats,
+        op_kwargs={"object_types": object_types + ["organizations"]},
+    )
+
+    send_stats_to_minio = PythonOperator(
+        task_id="send_stats_to_minio",
+        python_callable=send_stats_to_minio,
+    )
+
     get_perimeter_orgas.set_upstream(clean_previous_outputs)
+    get_perimeter_stats_organizations.set_upstream(get_perimeter_orgas)
+    for obj in object_types:
+        tasks[obj][0].set_upstream(get_perimeter_orgas)
+        tasks[obj][1].set_upstream(tasks[obj][0])
+        gather_stats.set_upstream(tasks[obj][1])
+    gather_stats.set_upstream(get_perimeter_stats_organizations)
+    send_stats_to_minio.set_upstream(gather_stats)
--- verticales/culture/task_functions.py
+++ verticales/culture/task_functions.py
@@ -1,7 +1,139 @@
+import json
+import logging
+
+import pandas as pd
+import requests
+
+from datagouvfr_data_pipelines.config import (
+    AIRFLOW_DAG_TMP,
+    AIRFLOW_ENV,
+    MINIO_BUCKET_DATA_PIPELINE_OPEN,
+)
+from datagouvfr_data_pipelines.utils.datagouv import (
+    get_all_from_api_query,
+    local_client,
+    DATAGOUV_SECRET_API_KEY,
+)
+from datagouvfr_data_pipelines.utils.filesystem import File
 from datagouvfr_data_pipelines.utils.grist import GristTable
+from datagouvfr_data_pipelines.utils.minio import MinIOClient
+
+DATADIR = f"{AIRFLOW_DAG_TMP}culture/data/"
+
+topic_id = "68889f00bd51536864e35316" if AIRFLOW_ENV == "prod" else "689604546058bf73a6c7a4eb"
+parquet_bucket = "hydra" if AIRFLOW_ENV == "prod" else "demo-hydra"
+metrics_api_url = "https://metric-api.data.gouv.fr/api/{}/data/?{}_id__exact={}"
+minio_open = MinIOClient(bucket=MINIO_BUCKET_DATA_PIPELINE_OPEN)
+
+objects = {
+    "datasets": {
+        "catalog_id": "f868cca6-8da1-4369-a78d-47463f19a9a3",
+        "metrics_keys": {"monthly_visit": "visits", "monthly_download_resource": "downloads"}
+    },
+    "dataservices": {
+        "catalog_id": "322d1475-f36a-472d-97ce-d218c8f79092",
+        "metrics_keys": {"monthly_visit": "visits"}
+    },
+    "reuses": {
+        "catalog_id": "970aafa0-3778-4d8b-b9d1-de937525e379",
+        "metrics_keys": {"monthly_visit": "visits"}
+    },
+    "organizations": {
+        # no need for the catalog, it's based on grist for the perimeter
+        "metrics_keys": {"monthly_visit_dataset": "visits"}
+    },
+}
 
 
 def get_perimeter_orgas(ti):
-    table = GristTable("hrDZg8StuE1d", "Perimetre_culture")
-    print(table["datagouv_id"].to_list())
-    ti.xcom_push(key="orgas", value=table["datagouv_id"].to_list())
+    table = GristTable("hrDZg8StuE1d", "Perimetre_Culture").to_dataframe()
+    ti.xcom_push(key="organizations", value=table["datagouv_id"].to_list())
+
+
+def get_and_send_perimeter_objects(ti, object_type: str):
+    orgas = ti.xcom_pull(key="organizations", task_ids="get_perimeter_orgas")
+    catalog_ids = pd.read_parquet(
+        f"https://object.files.data.gouv.fr/{parquet_bucket}-parquet/"
+        f"hydra-parquet/{objects[object_type]['catalog_id']}.parquet",
+        columns=["id", "organization_id"],
+        filters=[("organization_id", "in", orgas)],
+    )["id"].to_list()
+    # getting tags to put them back
+    tags = requests.get(
+        f"{local_client.base_url}/api/1/topics/{topic_id}/",
+        headers={"X-API-KEY": DATAGOUV_SECRET_API_KEY, "X-fields": "tags"},
+    ).json()["tags"]
+    # replacing topic field with fresh data
+    r = requests.put(
+        f"{local_client.base_url}/api/1/topics/{topic_id}/",
+        headers={"X-API-KEY": DATAGOUV_SECRET_API_KEY},
+        json={object_type: catalog_ids, "tags": tags},
+    )
+    r.raise_for_status()
+    ti.xcom_push(key=object_type, value=catalog_ids)
+
+
+def get_perimeter_stats(ti, object_type: str):
+    ids = ti.xcom_pull(
+        key=object_type,
+        task_ids=(
+            "get_perimeter_orgas" if object_type == "organizations"
+            else f"get_and_send_perimeter_{object_type}"
+        ),
+    )
+    stats = []
+    # we have to make a pirouette to end up with standardized labels
+    total = {
+        metric_label: 0 for metric_label in objects[object_type]["metrics_keys"].values()
+    } | {f"nb_{object_type}": 0}
+    for idx, obj_id in enumerate(ids):
+        if idx and idx % 20 == 0:
+            logging.info(f"> Got stats for {idx}/{len(ids)} {object_type}")
+        total[f"nb_{object_type}"] += 1
+        obj_stats = {metric_label: 0 for metric_label in objects[object_type]["metrics_keys"].values()}
+        for monthly_stats in get_all_from_api_query(
+            metrics_api_url.format(object_type, object_type[:-1], obj_id),
+            next_page="links.next",
+        ):
+            for metric_id in objects[object_type]["metrics_keys"].keys():
+                obj_stats[objects[object_type]["metrics_keys"][metric_id]] += monthly_stats[metric_id] or 0
+                total[objects[object_type]["metrics_keys"][metric_id]] += monthly_stats[metric_id] or 0
+        stats.append({"id": obj_id} | obj_stats)
+
+    ti.xcom_push(key=f"detailed_{object_type}", value=stats)
+    ti.xcom_push(key=f"total_{object_type}", value=total)
+
+
+def gather_stats(ti, object_types: list[str]):
+    detailed, total = {}, {}
+    for object_type in object_types:
+        detailed[object_type] = ti.xcom_pull(
+            key=f"detailed_{object_type}",
+            task_ids=f"get_perimeter_stats_{object_type}",
+        )
+        total[object_type] = ti.xcom_pull(
+            key=f"total_{object_type}",
+            task_ids=f"get_perimeter_stats_{object_type}",
+        )
+
+    with open(DATADIR + "detailed.json", "w") as f:
+        json.dump(detailed, f)
+
+    with open(DATADIR + "total.json", "w") as f:
+        json.dump(total, f)
+
+
+def send_stats_to_minio():
+    minio_open.send_files(
+        list_files=[
+            File(
+                source_path=DATADIR,
+                source_name=f"{scope}.json",
+                dest_path="verticale_culture/",
+                dest_name=f"{scope}.json",
+            )
+            for scope in ["detailed", "total"]
+        ],
+        ignore_airflow_env=True,
+        burn_after_sending=True,
+    )
