c80c64eb487ae3f042f0607451f009908f2c2f2e
Caillou <valentin.shamsnejad@numerique.gouv.fr>
2025-08-12 09:49:15 +0200

    run ruff format topics_manager.py

--- verticales/simplifions/topics_manager.py
+++ verticales/simplifions/topics_manager.py
@@ -55,7 +55,7 @@ class TopicsManager:
 
     @staticmethod
     def _nested_pop(dictionary, key_chain):
-        keys = key_chain.split('.')
+        keys = key_chain.split(".")
         current = dictionary
         for index, key in enumerate(keys):
             if index == len(keys) - 1:
@@ -67,14 +67,20 @@ class TopicsManager:
                 raise KeyError(f"Key {key} of {key_chain} not found in : {current}")
         return current
 
-    def _topics_are_similar_so_we_can_skip_update(self, old_topic: dict, new_topic: dict, ignore_topics_references: bool = True):
+    def _topics_are_similar_so_we_can_skip_update(
+        self, old_topic: dict, new_topic: dict, ignore_topics_references: bool = True
+    ):
         for key in ["name", "description", "tags", "private"]:
             if old_topic[key] != new_topic[key]:
-                logging.info(f"{key} is different : {old_topic[key]} != {new_topic[key]}")
+                logging.info(
+                    f"{key} is different : {old_topic[key]} != {new_topic[key]}"
+                )
                 return False
 
         if old_topic["organization"]["id"] != new_topic["organization"]["id"]:
-            logging.info(f"organization is different : {old_topic['organization']['id']} != {new_topic['organization']['id']}")
+            logging.info(
+                f"organization is different : {old_topic['organization']['id']} != {new_topic['organization']['id']}"
+            )
             return False
 
         if ignore_topics_references:
@@ -121,8 +127,12 @@ class TopicsManager:
         r.raise_for_status()
         logging.info(f"Updated topic at {url}")
 
-    def _update_topic_if_needed(self, topic: dict, topic_data: dict, ignore_topics_references: bool = True):
-        if self._topics_are_similar_so_we_can_skip_update(topic, topic_data, ignore_topics_references):
+    def _update_topic_if_needed(
+        self, topic: dict, topic_data: dict, ignore_topics_references: bool = True
+    ):
+        if self._topics_are_similar_so_we_can_skip_update(
+            topic, topic_data, ignore_topics_references
+        ):
             logging.info(f"Topic {topic['slug']} hasn't changed, skipping update")
             return
         self._update_topic_by_id(topic["id"], topic_data)
@@ -135,17 +145,20 @@ class TopicsManager:
         if topic["extras"] == new_extras:
             logging.info(f"Topic {topic['slug']} hasn't changed, skipping update")
             return
-        self._update_topic_by_id(topic["id"], {
-            "tags": topic["tags"],
-            "extras": new_extras,
-        })
+        self._update_topic_by_id(
+            topic["id"],
+            {
+                "tags": topic["tags"],
+                "extras": new_extras,
+            },
+        )
 
     def _get_all_topics_for_tag(self, tag: str) -> list[dict]:
         return get_all_from_api_query(
             f"{self.client.base_url}/api/1/topics/?tag={tag}&include_private=true",
             auth=True,
         )
-    
+
     def update_topics(self, ti):
         tag_and_grist_topics: dict = ti.xcom_pull(
             key="tag_and_grist_topics", task_ids="get_and_format_grist_data"
@@ -178,29 +191,42 @@ class TopicsManager:
                 topic_data = {
                     "name": grist_topics[simplifions_slug][title_column],
                     # description cannot be empty
-                    "description": grist_topics[simplifions_slug]["Description_courte"] or "-",
+                    "description": grist_topics[simplifions_slug]["Description_courte"]
+                    or "-",
                     "organization": {
                         "class": "Organization",
                         "id": "57fe2a35c751df21e179df72",
                     },
                     "tags": topic_tags
                     + self._generated_search_tags(grist_topics[simplifions_slug]),
-                    "extras": {extras_nested_key: grist_topics[simplifions_slug] or False},
-                    "private": not grist_topics[simplifions_slug]["Visible_sur_simplifions"],
+                    "extras": {
+                        extras_nested_key: grist_topics[simplifions_slug] or False
+                    },
+                    "private": not grist_topics[simplifions_slug][
+                        "Visible_sur_simplifions"
+                    ],
                 }
 
                 # Create or update the topic
                 if simplifions_slug in current_topics_by_simplifions_slug.keys():
-                    logging.info(f"Updating topic {simplifions_slug} : {old_topic['id']}")
-                    self._update_topic_if_needed(old_topic, topic_data, ignore_topics_references=True)
+                    logging.info(
+                        f"Updating topic {simplifions_slug} : {old_topic['id']}"
+                    )
+                    self._update_topic_if_needed(
+                        old_topic, topic_data, ignore_topics_references=True
+                    )
                 else:
-                    logging.info(f"Creating topic {simplifions_slug} : {topic_data['name']}")
+                    logging.info(
+                        f"Creating topic {simplifions_slug} : {topic_data['name']}"
+                    )
                     self._create_topic(topic_data)
 
             # deleting topics that are not in the table anymore
             for simplifions_slug in current_topics_by_simplifions_slug:
                 if simplifions_slug not in grist_topics:
-                    logging.info(f"Deleting topic {simplifions_slug} : {old_topic['id']}")
+                    logging.info(
+                        f"Deleting topic {simplifions_slug} : {old_topic['id']}"
+                    )
                     self._delete_topic(old_topic["id"])
 
     def update_topics_references(self, ti):
@@ -237,7 +263,9 @@ class TopicsManager:
                 "cas_d_usages_topics_ids"
             ] = [topic["id"] for topic in matching_topics]
             # update the solution topic with the new extras
-            self._update_extras_of_topic_if_needed(solution_topic, solution_topic["extras"])
+            self._update_extras_of_topic_if_needed(
+                solution_topic, solution_topic["extras"]
+            )
 
         # Update cas_usages_topics with references to solution_topic_id and solutions_editeurs_topics
         for cas_usage_topic in cas_usages_topics:
@@ -277,4 +305,6 @@ class TopicsManager:
                 ]
 
             # update the cas_usage topic with the new extras
-            self._update_extras_of_topic_if_needed(cas_usage_topic, cas_usage_topic["extras"])
+            self._update_extras_of_topic_if_needed(
+                cas_usage_topic, cas_usage_topic["extras"]
+            )
