8dfe59691fd7c50c12c68fe4b7aa37b3bdc7404a
Pierlou <pierlou.ramade@data.gouv.fr>
2025-11-24 15:03:59 +0100

    fix: irve

--- utils/schema.py
+++ utils/schema.py
@@ -23,6 +23,7 @@ from datagouvfr_data_pipelines.utils.datagouv import (
     ORGA_REFERENCE,
     VALIDATA_BASE_URL,
     local_client,
+    prod_client,
 )
 from datagouvfr_data_pipelines.utils.mattermost import send_message
 from datagouvfr_data_pipelines.utils.minio import File, MinIOClient
@@ -30,7 +31,7 @@ from datagouvfr_data_pipelines.utils.retry import simple_connection_retry
 
 pd.set_option("display.max_columns", None)
 tqdm.pandas(desc="pandas progress bar", mininterval=30)
-
+local_client = prod_client
 VALIDATA_BASE_URL = VALIDATA_BASE_URL + "/validate?schema={schema_url}&url={rurl}"
 MINIMUM_VALID_RESOURCES_TO_CONSOLIDATE = 5
 api_url = f"{local_client.base_url}/api/1/"
@@ -212,6 +213,9 @@ def parse_api(
         tmp = []
         for d in all_datasets:
             r = session.get(api_url + f"datasets/{d['id']}/")
+            if r.status_code == 404:
+                # skipping private datasets
+                continue
             r.raise_for_status()
             tmp.append(r.json())
         session.close()
