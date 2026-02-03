c7184534a98fdaa5ac937627c0f03eb20f8b2f13
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2026-02-03 17:34:05 +0100

    Refactor elections aggregation (#626)

--- data_processing/elections/aggregation/DAG.py
+++ data_processing/elections/aggregation/DAG.py
@@ -3,22 +3,16 @@ from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-from datagouvfr_data_pipelines.config import (
-    AIRFLOW_DAG_HOME,
-    AIRFLOW_DAG_TMP,
-)
 from datagouvfr_data_pipelines.data_processing.elections.aggregation.task_functions import (
-    format_election_files,
+    DATADIR,
     process_election_data,
     send_results_to_s3,
     publish_results_elections,
     send_notification,
 )
 
-TMP_FOLDER = f"{AIRFLOW_DAG_TMP}elections/"
 DAG_FOLDER = "datagouvfr_data_pipelines/data_processing/"
 DAG_NAME = "data_processing_elections"
-DATADIR = f"{AIRFLOW_DAG_TMP}elections/data"
 
 default_args = {
     "email": ["pierlou.ramade@data.gouv.fr", "geoffrey.aldebert@data.gouv.fr"],
@@ -36,20 +30,7 @@ with DAG(
 ) as dag:
     clean_previous_outputs = BashOperator(
         task_id="clean_previous_outputs",
-        bash_command=f"rm -rf {TMP_FOLDER} && mkdir -p {TMP_FOLDER}",
-    )
-
-    download_elections_data = BashOperator(
-        task_id="download_elections_data",
-        bash_command=(
-            f"bash {AIRFLOW_DAG_HOME}{DAG_FOLDER}"
-            f"elections/aggregation/scripts/script_dl_elections.sh {DATADIR} "
-        ),
-    )
-
-    format_election_files = PythonOperator(
-        task_id="format_election_files",
-        python_callable=format_election_files,
+        bash_command=f"rm -rf {DATADIR} && mkdir -p {DATADIR}",
     )
 
     process_election_data = PythonOperator(
@@ -69,7 +50,7 @@ with DAG(
 
     clean_up = BashOperator(
         task_id="clean_up",
-        bash_command=f"rm -rf {TMP_FOLDER}",
+        bash_command=f"rm -rf {DATADIR}",
     )
 
     send_notification = PythonOperator(
@@ -77,9 +58,7 @@ with DAG(
         python_callable=send_notification,
     )
 
-    download_elections_data.set_upstream(clean_previous_outputs)
-    format_election_files.set_upstream(download_elections_data)
-    process_election_data.set_upstream(format_election_files)
+    process_election_data.set_upstream(clean_previous_outputs)
     send_results_to_s3.set_upstream(process_election_data)
     publish_results_elections.set_upstream(send_results_to_s3)
     clean_up.set_upstream(publish_results_elections)
