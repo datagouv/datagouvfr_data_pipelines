0016da0c6ddd7eab0d88581f6f907d4bc6a4b170
Pierlou <pierlou.ramade@data.gouv.fr>
2026-02-12 12:40:59 +0100

    refactor: use new syntax

--- data_processing/meteo/hydra_and_previz/DAG.py
+++ data_processing/meteo/hydra_and_previz/DAG.py
@@ -1,12 +1,9 @@
 # from datetime import timedelta, datetime
 # from airflow import DAG
-# from airflow.operators.python import PythonOperator
 
 # from datagouvfr_data_pipelines.data_processing.meteo.hydra_and_previz.task_functions import (
 #     get_and_send_errors,
 # )
-
-# DAG_NAME = "data_processing_meteo_hydra_and_previz"
 
 # default_args = {
 #     "retries": 5,
@@ -14,7 +11,7 @@
 # }
 
 # with DAG(
-#     dag_id=DAG_NAME,
+#     dag_id="data_processing_meteo_hydra_and_previz",
 #     schedule="5 4 * * 1",
 #     start_date=datetime(2024, 6, 1),
 #     catchup=False,
@@ -22,9 +19,5 @@
 #     tags=["data_processing", "meteo", "hydra"],
 #     default_args=default_args,
 # ):
-#     get_and_send_errors = PythonOperator(
-#         task_id="get_and_send_errors",
-#         python_callable=get_and_send_errors,
-#     )
 
-#     get_and_send_errors
+#     get_and_send_errors()
