a4808be983c72aae147e2de2a31abe6fa4b60a12
Hadrien Bossard <hadrien.bossard.ext@mail.numerique.gouv.fr>
2026-03-11 16:43:36 +0100

    fix(geocodage): use debian user between dataeng and files.data

--- data_processing/insee/sirene/geocodage/scripts/7_rsync_to_files_data_gouv.sh
+++ data_processing/insee/sirene/geocodage/scripts/7_rsync_to_files_data_gouv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -a --rsync-path="mkdir -p /srv/nfs/files.data.gouv.fr/geo-sirene/$(date +%Y-%m) && rsync" /srv/sirene/data-sirene/$(date +%Y-%m)/ root@files.data.gouv.fr:/srv/nfs/files.data.gouv.fr/geo-sirene/$(date +%Y-%m)/
+rsync -a --rsync-path="mkdir -p /srv/nfs/files.data.gouv.fr/geo-sirene/$(date +%Y-%m) && rsync" /srv/sirene/data-sirene/$(date +%Y-%m)/ debian@files.data.gouv.fr:/srv/nfs/files.data.gouv.fr/geo-sirene/$(date +%Y-%m)/
