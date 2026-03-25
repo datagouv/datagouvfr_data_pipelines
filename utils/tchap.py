e51fb335342af43f98d4821a6f2f3e380fce46f9
Pierlou <pierlou.ramade@data.gouv.fr>
2026-03-25 15:27:52 +0100

    feat: create a function to ping on tchap

--- utils/tchap.py
+++ utils/tchap.py
@@ -1,10 +1,9 @@
-import logging
-
 import requests
 
 from datagouvfr_data_pipelines.config import (
     AIRFLOW_ENV,
-    TCHAP_DATAGOUV_WEBHOOK,
+    TCHAP_BASE_URL,
+    TCHAP_BOT_TOKEN,
     TCHAP_ROOM_DATAENG,
     TCHAP_ROOM_DATAENG_TEST,
 )
@@ -12,27 +11,22 @@ from datagouvfr_data_pipelines.config import (
 
 def send_message(
     text: str,
-    endpoint_url: str | None = None,
+    room_id: str | None = None,
 ) -> None:
-    """Send a message to a Tchap channel.
-
-    Args:
-        endpoint_url (str): URL of the Tchap endpoint (for bot)
-        text (str): Text to send to a channel
-        image_url (Optional[str], optional): Url of an image to link
-        with your text. Defaults to None.
-    """
-    if not endpoint_url:
-        endpoint_url = (
-            TCHAP_ROOM_DATAENG
-            if AIRFLOW_ENV == "prod"
-            else TCHAP_ROOM_DATAENG_TEST
-        )
-    data = {"roomId": TCHAP_ANNUAIRE_ROOM_ID, "message": text}
-
-    try:
-        response = requests.post(endpoint_url, json=data)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to send message: {e}")
-        raise Exception(f"Failed to send message: {e}")
+    """Send a message to a Tchap channel."""
+    room_id = room_id or (
+        TCHAP_ROOM_DATAENG
+        if AIRFLOW_ENV == "prod"
+        else TCHAP_ROOM_DATAENG_TEST
+    )
+    r = requests.post(
+        f"{TCHAP_BASE_URL}/_matrix/client/v3/rooms/{room_id}/send/m.room.message?access_token={TCHAP_BOT_TOKEN}",
+        headers={"content-type": "application/json"},
+        json={
+          "msgtype": "m.text",
+          "body": "_",
+          "format": "org.matrix.custom.html",
+          "formatted_body": text,
+        },
+    )
+    r.raise_for_status()
