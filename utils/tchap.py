8dd53a8565b8a7a215b66dfe30d528a7f25684aa
Pierlou <pierlou.ramade@data.gouv.fr>
2026-03-30 15:05:45 +0200

    feat: add tchap util

--- utils/tchap.py
+++ utils/tchap.py
@@ -1,3 +1,4 @@
+from markdown import markdown
 import requests
 
 from datagouvfr_data_pipelines.config import (
@@ -8,25 +9,61 @@ from datagouvfr_data_pipelines.config import (
     TCHAP_ROOM_DATAENG_TEST,
 )
 
+map_ping = {
+    "geoffrey": "geoffrey.aldebert-data.gouv.fr",
+    "hadrien": "hadrien.bossard.ext-mail.numerique.gouv.fr",
+    "pierlou": "pierlou.ramade-data.gouv.fr",
+    "valentin": "valentin.shamsnejad-beta.gouv.fr",
+    "alexandre": "alexandre.bulte.ext-numerique.gouv.fr",
+    "ludine": "ludine.pierquin.ext-mail.numerique.gouv.fr",
+    "antonin": "antonin.garrone-data.gouv.fr",
+}
+
+
+def markdown_to_html(md: str) -> str:
+    return markdown.markdown(md).replace("\n", "")
+
 
 def send_message(
     text: str,
-    room_id: str | None = None,
-) -> None:
-    """Send a message to a Tchap channel."""
-    room_id = room_id or (
+    room_id: str = (
         TCHAP_ROOM_DATAENG
         if AIRFLOW_ENV == "prod"
         else TCHAP_ROOM_DATAENG_TEST
-    )
+    ),
+    *,
+    message_type: str = "notice",  # whether to send a real message or an insight
+    ping: list[str] = [],
+) -> None:
+    """Send a message to a Tchap channel."""
+    assert message_type in {"text", "notice"}
+    if ping:
+        # notice doesn't ping
+        message_type = "text"
+    payload = {
+        "msgtype": f"m.{message_type}",
+        "body": "_",
+        "format": "org.matrix.custom.html",
+        "formatted_body": markdown_to_html(text),
+    }
+    if ping:
+        if ping[0] == "room":
+            payload["m.mentions"] = {
+                "room": True
+            }
+        else:
+            payload["m.mentions"] = {
+                "user_ids": [
+                    f"@{map_ping[name]}:agent.dinum.tchap.gouv.fr"
+                    for name in ping if name in map_ping
+                ],
+            }
     r = requests.post(
-        f"{TCHAP_BASE_URL}/_matrix/client/v3/rooms/{room_id}/send/m.room.message?access_token={TCHAP_BOT_TOKEN}",
-        headers={"content-type": "application/json"},
-        json={
-          "msgtype": "m.text",
-          "body": "_",
-          "format": "org.matrix.custom.html",
-          "formatted_body": text,
+        f"{TCHAP_BASE_URL}/_matrix/client/v3/rooms/{room_id}/send/m.room.message",
+        headers={
+            "content-type": "application/json",
+            "authorization": f"Bearer {TCHAP_BOT_TOKEN}",
         },
+        json=payload,
     )
     r.raise_for_status()
