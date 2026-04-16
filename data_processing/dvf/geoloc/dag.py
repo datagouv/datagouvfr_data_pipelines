5607cf38c21428e41a63edf28509773da176cdca
Pierlou <pierlou.ramade@data.gouv.fr>
2026-04-16 17:39:37 +0200

    feat: draft of migration to airflow

---
+++ data_processing/dvf/geoloc/dag.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import ShortCircuitOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datagouvfr_data_pipelines.data_processing.dvf.geoloc.task_functions import (
+    TMP_FOLDER,
+    check_if_modif,
+    download_source_data,
+    enrich_years,
+    publish_datagouv,
+    notification,
+)
+from datagouvfr_data_pipelines.utils.tasks import clean_up_folder
+
+with DAG(
+    dag_id="data_processing_dvf_geoloc",
+    schedule=None,
+    start_date=datetime(2026, 4, 15),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=300),
+    tags=["data_processing", "dvf"],
+):
+    (
+        enrich_years(
+            files=(
+                ShortCircuitOperator(
+                    task_id="check_if_modif",
+                    python_callable=check_if_modif,
+                )
+                >> clean_up_folder(TMP_FOLDER, recreate=True)
+                >> download_source_data()
+            )
+        ) >> publish_datagouv()
+        # >> notification()
+        # >> TriggerDagRunOperator(
+        #     task_id="trigger_dvf_explore",
+        #     trigger_dag_id="data_processing_dvf_explore",
+        # )
+    )
