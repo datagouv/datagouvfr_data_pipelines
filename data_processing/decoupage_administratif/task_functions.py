55f7b806a16d0e8bafd28cafe93bd1fa7ad80304
Thomas Gratier <thomas_gratier@yahoo.fr>
2026-05-12 14:26:01 +0200

    Improve and reformat code for decoupage-administratif

--- data_processing/decoupage_administratif/task_functions.py
+++ data_processing/decoupage_administratif/task_functions.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import mimetypes
-import re
 
 
 from airflow.decorators import task
@@ -10,7 +9,6 @@ from airflow.decorators import task
 from datagouvfr_data_pipelines.config import (
     AIRFLOW_ENV,
     AIRFLOW_DAG_HOME,
-    S3_URL_RBX,
 )
 from datagouvfr_data_pipelines.utils.datagouv import (
     local_client,
@@ -24,40 +22,33 @@ mimetypes.init()
 LATEST_YEAR = "2026"
 END_DATE = "2026-04-01"
 
-commons = [
-    'communes',
-    'epci',
-    'departements',
-    'regions',
-    'arrondissements'
-]
+commons = ["communes", "epci", "departements", "regions", "arrondissements"]
 commons_name = [
-    'communes dont communes associées/déléguées et arrondissements municipaux',
-    'EPCI',
-    'départements',
-    'régions',
-    'arrondissements'
+    "communes dont communes associées/déléguées et arrondissements municipaux",
+    "EPCI",
+    "départements",
+    "régions",
+    "arrondissements",
 ]
 commons_dict = dict(zip(commons, commons_name))
-commons_dict['ept'] = 'EPT'
+commons_dict["ept"] = "EPT"
 
 
 versions_prefix = {
-    '2019': '0.7',
-    '2020': '0.8',
-    '2021': '1.',
-    '2022': '2.',
-    '2023': '3.',
-    '2024': '4.',
-    '2025': '5.',
-    '2026': '6.'
+    "2019": "0.7",
+    "2020": "0.8",
+    "2021": "1.",
+    "2022": "2.",
+    "2023": "3.",
+    "2024": "4.",
+    "2025": "5.",
+    "2026": "6.",
 }
 
-base_unpkg_decoupage_administratif = 'https://unpkg.com/@etalab/decoupage-administratif'
+base_unpkg_decoupage_administratif = "https://unpkg.com/@etalab/decoupage-administratif"
 
 
-
-DAG_NAME = 'decoupage-administratif'
+DAG_NAME = "decoupage-administratif"
 DAG_FOLDER = "datagouvfr_data_pipelines/data_processing/"
 with open(f"{AIRFLOW_DAG_HOME}{DAG_FOLDER}decoupage_administratif/config.json") as fp:
     config = json.load(fp)
@@ -66,77 +57,105 @@ DATASET_ID = config[DAG_NAME][AIRFLOW_ENV]["dataset_id"]
 
 dataset = local_client.dataset(DATASET_ID)
 
+
 def npm_package_metadata(package_name):
-    url_npm_registry = f'https://registry.npmjs.org/{package_name}/'
+    url_npm_registry = f"https://registry.npmjs.org/{package_name}/"
     r_url_npm_registry = requests.get(url_npm_registry)
     return r_url_npm_registry.json()
 
+
 def npm_package_latest_version_for_years():
     versions = {}
-    metadata = npm_package_metadata('@etalab/decoupage-administratif')
-    npm_package_versions_without_alpha_beta = [i for i in metadata.get('versions').keys() if '-' not in i]
+    metadata = npm_package_metadata("@etalab/decoupage-administratif")
+    npm_package_versions_without_alpha_beta = [
+        i for i in metadata.get("versions").keys() if "-" not in i
+    ]
     for year, version_prefix in versions_prefix.items():
-        versions[year] = sorted([j for j in npm_package_versions_without_alpha_beta if j.startswith(version_prefix)])[-1]
+        versions[year] = sorted(
+            [
+                j
+                for j in npm_package_versions_without_alpha_beta
+                if j.startswith(version_prefix)
+            ]
+        )[-1]
     return versions
+
 
 NPM_PACKAGE_VERSIONS = npm_package_latest_version_for_years()
 
+
 def generate_files_infos_decoupage_administratif():
     output = []
+    priority_by_type = {i:float((idx + 1) * 10) for idx, i in enumerate(reversed(commons_dict.keys()))}
     for year, version in NPM_PACKAGE_VERSIONS.items():
-        type_file = 'main' if LATEST_YEAR in year else 'other'
-        mime_file = 'application/json'
-        format_file = 'json'
-        if version == '0.8.0':
-            mytitle = 'Données EPCI 2020'
-            url = (f'{base_unpkg_decoupage_administratif}@{version}/data/epci.json')
-            r = requests.get(url, stream=True, headers={'Accept-Encoding': None})
-            mysize = r.headers['Content-Length']
-            output.append({'title': mytitle, 'url': url, 'filesize': mysize, 'type': type_file, 'mime': mime_file, 'format': format_file})
+        type_file = "main" if LATEST_YEAR in year else "other"
+        mime_file = "application/json"
+        format_file = "json"
+        if version == "0.8.0":
+            priority = priority_by_type['epci'] + 1 / float(2020)
+            mytitle = "Données EPCI 2020"
+            url = f"{base_unpkg_decoupage_administratif}@{version}/data/epci.json"
+            r = requests.get(url, stream=True, headers={"Accept-Encoding": None})
+            mysize = r.headers["Content-Length"]
+            output.append(
+                {
+                    "title": mytitle,
+                    "url": url,
+                    "filesize": mysize,
+                    "type": type_file,
+                    "mime": mime_file,
+                    "format": format_file,
+                    "year": year,
+                    "priority": priority,
+                }
+            )
         elif int(year) >= 2023:
-            for name in reversed(commons + ['ept']):
-                url = (f'{base_unpkg_decoupage_administratif}@{version}/data/{name}.json')
-                mytitle = f'Données {commons_dict[name]} {year}'
-                r = requests.get(url, stream=True, headers={'Accept-Encoding': None})
-                mysize = r.headers['Content-Length']
-                output.append({'title': mytitle, 'url': url, 'filesize': mysize, 'type': type_file, 'mime': mime_file, 'format': format_file})
+            for name in reversed(commons + ["ept"]):
+                priority = priority_by_type[name] + 1 / float(year)
+                url = f"{base_unpkg_decoupage_administratif}@{version}/data/{name}.json"
+                mytitle = f"Données {commons_dict[name]} {year}"
+                r = requests.get(url, stream=True, headers={"Accept-Encoding": None})
+                mysize = r.headers["Content-Length"]
+                output.append(
+                    {
+                        "title": mytitle,
+                        "url": url,
+                        "filesize": mysize,
+                        "type": type_file,
+                        "mime": mime_file,
+                        "format": format_file,
+                        "year": year,
+                        "priority": priority,
+                    }
+                )
         else:
             for name in reversed(commons):
-                url = (f'{base_unpkg_decoupage_administratif}@{version}/data/{name}.json')
-                mytitle = f'Données {commons_dict[name]} {year}'
-                r = requests.get(url, stream=True, headers={'Accept-Encoding': None})
-                mysize = r.headers['Content-Length']
-                output.append({'title': mytitle, 'url': url, 'filesize': mysize, 'type': type_file, 'mime': mime_file, 'format': format_file})
+                priority = priority_by_type[name] + 1 / float(year)
+                url = f"{base_unpkg_decoupage_administratif}@{version}/data/{name}.json"
+                mytitle = f"Données {commons_dict[name]} {year}"
+                r = requests.get(url, stream=True, headers={"Accept-Encoding": None})
+                mysize = r.headers["Content-Length"]
+                output.append(
+                    {
+                        "title": mytitle,
+                        "url": url,
+                        "filesize": mysize,
+                        "type": type_file,
+                        "mime": mime_file,
+                        "format": format_file,
+                        "year": year,
+                        "priority": priority,
+                    }
+                )
     return output
-
-ressources_unpkg = generate_files_infos_decoupage_administratif()
-urls_res_s3 = [i.get('url') for i in ressources_unpkg]
-
-
-for res in dataset.resources:
-    if res.url in urls_res_mc:
-        print('Existing ressource')
-
-urls_res_dataset = [i.url for i in dataset.resources]
-
-for res_mc in ressources_unpkg:
-    if res_mc['url'] not in urls_res_dataset:
-        resource = client_datagouv.resource().create_remote(
-            payload={"url": res_mc['url'], "title": res_mc['title'], "size": res_mc['size'], "type": res_mc['type']},
-            dataset_id=dataset.id,
-        )
 
 
 @task
 def update_create_resources():
-    ressources_s3 = generate_files_infos_contours_administratifs(
-        get_files_info_from_s3()
-    )
-    urls_res_dataset = {
-        res.url[res.url.index("/20") + 1 :]: res for res in dataset.resources
-    }
-    for res in ressources_s3:
-        if res.get("s3_path") in urls_res_dataset:
+    ressources_unpkg = generate_files_infos_decoupage_administratif()
+    urls_res_dataset = {res.title: res for res in dataset.resources}
+    for res in ressources_unpkg:
+        if res.get("title") in urls_res_dataset:
             logging.info("Existing ressource. Only update")
             payload = {
                 "url": res.get("url"),
@@ -147,9 +166,9 @@ def update_create_resources():
                 "type": res.get("type"),
             }
             logging.info(payload)
-            urls_res_dataset[res.get("s3_path")].update(payload)
+            urls_res_dataset[res.get("title")].update(payload)
         else:
-            logging.info("Create new ressource", res.get("s3_path"))
+            logging.info("Create new ressource", res.get("url"))
             payload = {
                 "url": res.get("url"),
                 "title": res.get("title"),
@@ -165,12 +184,12 @@ def update_create_resources():
 @task
 def specific_sort_ressources():
     dataset_reloaded = local_client.dataset(DATASET_ID)
+    ressources_unpkg = generate_files_infos_decoupage_administratif()    
     urls_res_dataset_reloaded = {
-        res.url[res.url.index("/20") + 1 :]: res for res in dataset_reloaded.resources
+        res.url: res for res in dataset_reloaded.resources
     }
-    sorted_ids_res_datagouv = [
-        urls_res_dataset_reloaded[i].id for i in urls_res_dataset_reloaded
-    ]
+    # sorted_ids_res_datagouv = [urls_res_dataset_reloaded[j[0]].id for j in sorted([[i.get('url'), i.get('priority')] for i in ressources_unpkg], key=lambda x: x[1]) if j[0] in urls_res_dataset_reloaded]
+    sorted_ids_res_datagouv = [urls_res_dataset_reloaded[j[0]].id for j in sorted([[i.get('url'), i.get('priority')] for i in ressources_unpkg], key=lambda x: x[1]) if j[0]]
     local_client.session.put(
         dataset_reloaded.uri + "resources/",
         json=sorted_ids_res_datagouv,
@@ -179,22 +198,18 @@ def specific_sort_ressources():
 
 @task()
 def update_temporal_coverage():
-    dataset.update({
-        "temporal_coverage": {
-            "end": END_DATE,
-            "start": "2019-01-01"
-        }
-    })
+    dataset.update({"temporal_coverage": {"end": END_DATE, "start": "2019-01-01"}})
 
 
 @task()
 def notification():
     send_message(
         text=(
-            f"📣 Mise à jour de la liste des ressources sur data.gouv.fr pour les données {DAG_BUCKET_NAME}.\n\n"
-            f"- Données stockées sur S3 - Bucket {DAG_BUCKET_NAME}\n"
+            f"📣 Mise à jour de la liste des ressources sur data.gouv.fr pour les données {DAG_NAME}.\n\n"
+            f"- Données stockées sur npmjs - https://www.npmjs.com/package/@etalab/decoupage-administratif\n"
             f"- Données publiées [sur data.gouv.fr]({local_client.base_url}/datasets/{DATASET_ID}/)"
         )
     )
+
 
 NPM_PACKAGE_VERSIONS = npm_package_latest_version_for_years()
