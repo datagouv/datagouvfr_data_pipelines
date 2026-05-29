ba283e4ba6fd273beda41a40c42a6c9485cb44bb
Pierlou <pierlou.ramade@data.gouv.fr>
2026-05-29 18:37:35 +0200

    chore: lint

--- data_processing/sante/controle_sanitaire_eau/dag.py
+++ data_processing/sante/controle_sanitaire_eau/dag.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 
 from airflow.providers.standard.operators.python import ShortCircuitOperator
 from airflow.sdk import DAG
-from datagouvfr_data_pipelines.config import AIRFLOW_DAG_HOME
 from datagouvfr_data_pipelines.data_processing.sante.controle_sanitaire_eau.task_functions import (
     TMP_FOLDER,
     config,
